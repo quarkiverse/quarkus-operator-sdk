d29ced0d3e6440a36fb2db8de1b9f97a3265d62b
Chris Laprun <metacosm@gmail.com>
2021-03-12 19:44:54 +0100

    refactor: initial attempt at splitting the operator configuration
    The goal is to be able to configure the controllers at runtime as well,
    in particular which namespaces they need to monitor. Currently doesn't
    work as we cannot inject runtime configuration.

---
+++ runtime/src/main/java/io/quarkiverse/operatorsdk/runtime/OperatorRunTimeConfiguration.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ *
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.quarkiverse.operatorsdk.runtime;
+
+import java.util.Map;
+
+import io.quarkus.runtime.annotations.ConfigItem;
+import io.quarkus.runtime.annotations.ConfigPhase;
+import io.quarkus.runtime.annotations.ConfigRoot;
+
+@ConfigRoot(name = "operator-sdk", phase = ConfigPhase.RUN_TIME)
+public class OperatorRunTimeConfiguration {
+
+    /**
+     * Maps a controller name to its configuration.
+     */
+    @ConfigItem
+    public Map<String, ExternalControllerConfiguration> controllers;
+}
