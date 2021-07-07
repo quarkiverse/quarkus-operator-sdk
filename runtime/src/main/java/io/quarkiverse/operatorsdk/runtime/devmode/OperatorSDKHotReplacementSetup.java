339e5028b9e07d62cf0ec46f29c67f9760c2b4bb
Chris Laprun <metacosm@gmail.com>
2021-07-07 22:51:20 +0200

    feat: force scan every 10 seconds

---
+++ runtime/src/main/java/io/quarkiverse/operatorsdk/runtime/devmode/OperatorSDKHotReplacementSetup.java
@@ -0,0 +1,23 @@
+package io.quarkiverse.operatorsdk.runtime.devmode;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import io.quarkus.dev.spi.HotReplacementContext;
+import io.quarkus.dev.spi.HotReplacementSetup;
+
+public class OperatorSDKHotReplacementSetup implements HotReplacementSetup {
+    private static final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+
+    @Override
+    public void setupHotDeployment(HotReplacementContext context) {
+        executor.scheduleAtFixedRate(() -> {
+            try {
+                System.out.println("Reload: " + context.doScan(false));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }, 10, 10, TimeUnit.SECONDS);
+    }
+}
