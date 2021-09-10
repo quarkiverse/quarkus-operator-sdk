25bc1078b0e83dcb1542578c781bcb1d80f04d8e
Chris Laprun <metacosm@gmail.com>
2021-09-10 15:56:33 +0200

    feat: start setting things up for CSV generation

--- deployment/src/main/java/io/quarkiverse/operatorsdk/deployment/CRDGeneration.java
+++ deployment/src/main/java/io/quarkiverse/operatorsdk/deployment/CRDGeneration.java
@@ -1,12 +1,19 @@
 package io.quarkiverse.operatorsdk.deployment;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.*;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
 
 import io.fabric8.crd.generator.CRDGenerator;
 import io.fabric8.crd.generator.CustomResourceInfo;
 import io.fabric8.kubernetes.client.CustomResource;
+import io.fabric8.openshift.api.model.operatorhub.v1alpha1.ClusterServiceVersionBuilder;
 import io.quarkiverse.operatorsdk.runtime.CRDConfiguration;
 import io.quarkiverse.operatorsdk.runtime.CRDGenerationInfo;
 import io.quarkiverse.operatorsdk.runtime.CRDInfo;
@@ -16,6 +23,17 @@ class CRDGeneration {
     private final CRDGenerator generator = new CRDGenerator();
     private final boolean generate;
     private boolean needGeneration;
+    private final CustomResourceControllerMapping crMappings = new CustomResourceControllerMapping();
+
+    private static final ObjectMapper YAML_MAPPER;
+    static {
+        YAML_MAPPER = new ObjectMapper((new YAMLFactory()).enable(YAMLGenerator.Feature.MINIMIZE_QUOTES)
+                .enable(YAMLGenerator.Feature.ALWAYS_QUOTE_NUMBERS_AS_STRINGS)
+                .disable(YAMLGenerator.Feature.WRITE_DOC_START_MARKER));
+        YAML_MAPPER.configure(SerializationFeature.INDENT_OUTPUT, true);
+        YAML_MAPPER.configure(SerializationFeature.WRITE_NULL_MAP_VALUES, false);
+        YAML_MAPPER.configure(SerializationFeature.WRITE_EMPTY_JSON_ARRAYS, false);
+    }
 
     public CRDGeneration(boolean generate) {
         this.generate = generate;
@@ -50,9 +68,23 @@ class CRDGeneration {
             final var info = generator.forCRDVersions(crdConfig.versions).inOutputDir(outputDir).detailedGenerate();
             final var crdDetailsPerNameAndVersion = info.getCRDDetailsPerNameAndVersion();
 
+            final var controllerToCSVBuilders = new HashMap<String, ClusterServiceVersionBuilder>(7);
+
             crdDetailsPerNameAndVersion.forEach((crdName, initialVersionToCRDInfoMap) -> {
                 OperatorSDKProcessor.log.infov("Generated {0} CRD:", crdName);
                 generated.add(crdName);
+
+                final var versions = crMappings.getCustomResourceInfos(crdName);
+                versions.forEach((version, cri) -> controllerToCSVBuilders
+                        .computeIfAbsent(cri.getControllerName(), s -> new ClusterServiceVersionBuilder()
+                                .withNewMetadata().withName(s).endMetadata())
+                        .editOrNewSpec()
+                        .editOrNewCustomresourcedefinitions()
+                        .addNewOwned()
+                        .withName(crdName)
+                        .withVersion(version)
+                        .withKind(cri.getKind())
+                        .endOwned().endCustomresourcedefinitions().endSpec());
 
                 final var versionToCRDInfo = converted.computeIfAbsent(crdName, s -> new HashMap<>());
                 initialVersionToCRDInfoMap
@@ -60,15 +92,27 @@ class CRDGeneration {
                             final var filePath = crdInfo.getFilePath();
                             OperatorSDKProcessor.log.infov("  - {0} -> {1}", version, filePath);
                             versionToCRDInfo.put(version, new CRDInfo(crdInfo.getCrdName(),
-                                    version, filePath, crdInfo.getDependentClassNames()));
+                                    version, filePath, crdInfo.getDependentClassNames(), versions));
                         });
+            });
+
+            controllerToCSVBuilders.forEach((controllerName, csvBuilder) -> {
+                final var csv = csvBuilder.build();
+                try {
+                    final var outputStream = new FileOutputStream(new File(outputDir, controllerName + ".csv.yml"));
+                    YAML_MAPPER.writeValue(outputStream, csv);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
             });
         }
         return new CRDGenerationInfo(crdConfig.apply, validateCustomResources, converted, generated);
     }
 
-    public void withCustomResource(Class<CustomResource> crClass, String crdName) {
-        generator.customResources(CustomResourceInfo.fromClass(crClass));
+    public void withCustomResource(Class<CustomResource> crClass, String crdName, String controllerName) {
+        final var info = CustomResourceInfo.fromClass(crClass);
+        crMappings.add(info, crdName, controllerName);
+        generator.customResources(info);
         needGeneration = true;
     }
 }
---
+++ deployment/src/main/java/io/quarkiverse/operatorsdk/deployment/CustomResourceControllerMapping.java
@@ -0,0 +1,26 @@
+package io.quarkiverse.operatorsdk.deployment;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import io.quarkiverse.operatorsdk.runtime.CustomResourceInfo;
+
+public class CustomResourceControllerMapping {
+    private final Map<String, Map<String, CustomResourceInfo>> crdNameToCRVersionToCRInfos = new HashMap<>(7);
+
+    public Map<String, CustomResourceInfo> getCustomResourceInfos(String crdName) {
+        final var customResourceInfos = crdNameToCRVersionToCRInfos.get(crdName);
+        if (customResourceInfos == null) {
+            throw new IllegalStateException("Should have information associated with '" + crdName + "'");
+        }
+        return customResourceInfos;
+    }
+
+    public void add(io.fabric8.crd.generator.CustomResourceInfo info, String crdName, String controllerName) {
+        final var converted = new CustomResourceInfo(
+                info.group(), info.version(), info.kind(), info.singular(), info.plural(), info.shortNames(), info.storage(),
+                info.served(), info.scope(), info.crClassName(),
+                info.specClassName(), info.statusClassName(), controllerName);
+        crdNameToCRVersionToCRInfos.computeIfAbsent(crdName, s -> new HashMap<>()).put(info.version(), converted);
+    }
+}
