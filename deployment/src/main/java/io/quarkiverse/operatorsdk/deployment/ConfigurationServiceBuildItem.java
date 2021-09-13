026659f32f802d680d254c15e4d7d36cc21af060
Chris Laprun <metacosm@gmail.com>
2021-09-13 17:10:31 +0200

    refactor: attempt to generate CSVs after k8s manifests are created
    This is a sync commit.
    
    The idea is that we need to be able to read any existing roles or
    deployments to add that information to the generated CSV. However, so
    far, either the CSV generation occurs before the manifests are
    generated, doesn't occur at all or introduces a build item cycle (which
    is the case with this specific version of the code).

--- deployment/src/main/java/io/quarkiverse/operatorsdk/deployment/ConfigurationServiceBuildItem.java
+++ deployment/src/main/java/io/quarkiverse/operatorsdk/deployment/ConfigurationServiceBuildItem.java
@@ -2,7 +2,6 @@ package io.quarkiverse.operatorsdk.deployment;
 
 import java.util.List;
 
-import io.quarkiverse.operatorsdk.runtime.CRDGenerationInfo;
 import io.quarkiverse.operatorsdk.runtime.QuarkusControllerConfiguration;
 import io.quarkiverse.operatorsdk.runtime.Version;
 import io.quarkus.builder.item.SimpleBuildItem;
@@ -11,13 +10,10 @@ public final class ConfigurationServiceBuildItem extends SimpleBuildItem {
 
     private final Version version;
     private final List<QuarkusControllerConfiguration> controllerConfigs;
-    private final CRDGenerationInfo crdInfo;
 
-    public ConfigurationServiceBuildItem(Version version,
-            List<QuarkusControllerConfiguration> controllerConfigs, CRDGenerationInfo crdInfo) {
+    public ConfigurationServiceBuildItem(Version version, List<QuarkusControllerConfiguration> controllerConfigs) {
         this.version = version;
         this.controllerConfigs = controllerConfigs;
-        this.crdInfo = crdInfo;
     }
 
     public Version getVersion() {
@@ -26,9 +22,5 @@ public final class ConfigurationServiceBuildItem extends SimpleBuildItem {
 
     public List<QuarkusControllerConfiguration> getControllerConfigs() {
         return controllerConfigs;
-    }
-
-    public CRDGenerationInfo getCRDGenerationInfo() {
-        return crdInfo;
     }
 }
---
+++ deployment/src/main/java/io/quarkiverse/operatorsdk/deployment/GeneratedCSVBuildItem.java
@@ -0,0 +1,6 @@
+package io.quarkiverse.operatorsdk.deployment;
+
+import io.quarkus.builder.item.SimpleBuildItem;
+
+public final class GeneratedCSVBuildItem extends SimpleBuildItem {
+}
