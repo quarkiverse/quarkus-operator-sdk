19e7c874c61ec0307f9e040cd1b0f52e6570ec4b
Chris Laprun <metacosm@gmail.com>
2021-10-19 23:15:42 +0200

    chore(deps): update to JOSDK 1.9.8 and Fabric8 5.8.0
    Infrastructure for testing delayed registration seems broken at the
    moment. Need to investigate.

---
+++ deployment/src/main/java/io/quarkiverse/operatorsdk/deployment/Constants.java
@@ -0,0 +1,14 @@
+package io.quarkiverse.operatorsdk.deployment;
+
+import io.fabric8.kubernetes.client.CustomResource;
+import io.javaoperatorsdk.operator.api.Controller;
+import io.javaoperatorsdk.operator.api.ResourceController;
+import io.javaoperatorsdk.operator.processing.ConfiguredController;
+import org.jboss.jandex.DotName;
+
+public class Constants {
+    public static final DotName RESOURCE_CONTROLLER = DotName.createSimple(ResourceController.class.getName());
+    public static final DotName CONFIGURED_CONTROLLER = DotName.createSimple(ConfiguredController.class.getName());
+    public static final DotName CUSTOM_RESOURCE = DotName.createSimple(CustomResource.class.getName());
+    public static final DotName CONTROLLER = DotName.createSimple(Controller.class.getName());
+}
