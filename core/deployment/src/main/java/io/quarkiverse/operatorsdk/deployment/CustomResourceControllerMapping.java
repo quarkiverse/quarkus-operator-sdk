e7f59c903bb0779a034d6ac98d8d2db11de6ca8d
Chris Laprun <metacosm@gmail.com>
2021-10-26 20:22:10 +0200

    fix: make sure we cannot register 2 controllers for the same CR

--- core/deployment/src/main/java/io/quarkiverse/operatorsdk/deployment/CustomResourceControllerMapping.java
+++ core/deployment/src/main/java/io/quarkiverse/operatorsdk/deployment/CustomResourceControllerMapping.java
@@ -17,10 +17,19 @@ public class CustomResourceControllerMapping {
     }
 
     public void add(io.fabric8.crd.generator.CustomResourceInfo info, String crdName, String associatedControllerName) {
+        final var version = info.version();
+        final var versionsForCR = crdNameToCRVersionToCRInfos.computeIfAbsent(crdName, s -> new HashMap<>());
+        final var cri = versionsForCR.get(version);
+        if (cri != null) {
+            throw new IllegalStateException("Cannot process controller '" + associatedControllerName +
+                    "' because a controller (" + cri.getControllerName() + ") is already associated with CRD "
+                    + crdName + " with version " + version);
+        }
+
         final var converted = new CustomResourceInfo(
-                info.group(), info.version(), info.kind(), info.singular(), info.plural(), info.shortNames(), info.storage(),
+                info.group(), version, info.kind(), info.singular(), info.plural(), info.shortNames(), info.storage(),
                 info.served(), info.scope(), info.crClassName(),
                 info.specClassName(), info.statusClassName(), crdName, associatedControllerName);
-        crdNameToCRVersionToCRInfos.computeIfAbsent(crdName, s -> new HashMap<>()).put(info.version(), converted);
+        versionsForCR.put(version, converted);
     }
 }
