8d6e67f94ae51bf07ef7dfe9f0dfb43abdc98680
Sébastien CROCQUESEL <88554524+scrocquesel@users.noreply.github.com>
2022-01-19 21:27:00 +0100

    test: add integration tests to joke sample (#197)

---
+++ samples/src/test/java/io/quarkiverse/operatorsdk/samples/joke/JokeReconcilerTest.java
@@ -0,0 +1,84 @@
+package io.quarkiverse.operatorsdk.samples.joke;
+
+import static java.util.concurrent.TimeUnit.MINUTES;
+import static org.awaitility.Awaitility.await;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
+import io.fabric8.kubernetes.client.server.mock.KubernetesServer;
+import io.quarkiverse.operatorsdk.samples.joke.JokeRequestSpec.Category;
+import io.quarkiverse.operatorsdk.samples.joke.JokeRequestStatus.State;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
+import io.quarkus.test.kubernetes.client.KubernetesTestServer;
+import io.quarkus.test.kubernetes.client.WithKubernetesTestServer;
+
+@WithKubernetesTestServer
+@QuarkusTest
+public class JokeReconcilerTest {
+
+    @InjectMock
+    @RestClient
+    JokeService jokeService;
+
+    @KubernetesTestServer
+    KubernetesServer mockServer;
+
+    @Inject
+    JokeReconciler test;
+
+    @Test
+    void CanReconcile() {
+
+        // arange
+        final JokeModel joke = new JokeModel();
+        joke.id = 1;
+        joke.joke = "Hello";
+        joke.flags = Map.of();
+
+        Mockito.when(jokeService.getRandom(eq(Category.Any), anyString(), anyBoolean(), anyString())).thenReturn(joke);
+
+        final JokeRequest testRequest = new JokeRequest();
+        testRequest.setMetadata(new ObjectMetaBuilder()
+                .withName("myjoke1")
+                .withNamespace(mockServer.getClient().getNamespace())
+                .build());
+        testRequest.setSpec(new JokeRequestSpec());
+        testRequest.getSpec().setCategory(Category.Any);
+
+        // act
+        mockServer.getClient().resources(JokeRequest.class).create(testRequest);
+
+        // assert
+        await().atMost(5, MINUTES).untilAsserted(() -> {
+            JokeRequest updatedRequest = mockServer.getClient().resources(JokeRequest.class)
+                    .inNamespace(testRequest.getMetadata().getNamespace())
+                    .withName(testRequest.getMetadata().getName()).get();
+            assertThat(updatedRequest.getStatus(), is(notNullValue()));
+            assertThat(updatedRequest.getStatus().getState(), equalTo(State.CREATED));
+        });
+
+        var createdJokes = mockServer.getClient().resources(Joke.class).inNamespace(testRequest.getMetadata().getNamespace())
+                .list();
+
+        assertThat(createdJokes.getItems(), is(not(empty())));
+        assertThat(createdJokes.getItems().get(0).getJoke(), is("Hello"));
+    }
+
+}
