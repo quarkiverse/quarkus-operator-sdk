8c2ec0876ae231a23282fe52c91b16f4c6e5dff9
Chris Laprun <metacosm@gmail.com>
2022-01-24 11:18:13 +0100

    feat: update SDK to 2.1.0-SNAPSHOT, also ignore DependentResourceManager
    Whenever an internal SDK implements Reconciler, we need to ignore it so
    that it's not considered by the extension as user-provided.
    
    See java-operator-sdk/java-operator-sdk#862

--- common/src/main/java/io/quarkiverse/operatorsdk/common/ClassUtils.java
+++ common/src/main/java/io/quarkiverse/operatorsdk/common/ClassUtils.java
@@ -1,6 +1,7 @@
 package io.quarkiverse.operatorsdk.common;
 
 import static io.quarkiverse.operatorsdk.common.Constants.CONTROLLER;
+import static io.quarkiverse.operatorsdk.common.Constants.DEPENDENT_RESOURCE_MANAGER;
 import static io.quarkiverse.operatorsdk.common.Constants.RECONCILER;
 
 import java.lang.reflect.Modifier;
@@ -23,17 +24,26 @@ public class ClassUtils {
         }
     }
 
+    /**
+     * Only retrieve {@link io.javaoperatorsdk.operator.api.reconciler.Reconciler} implementations that should be considered by
+     * the extension, excluding the SDK's own implementations.
+     * 
+     * @param index
+     * @param log
+     * @return
+     */
     public static Stream<ClassInfo> getKnownReconcilers(IndexView index, Logger log) {
         return index.getAllKnownImplementors(RECONCILER).stream().filter(ci -> keep(ci, log));
     }
 
     private static boolean keep(ClassInfo ci, Logger log) {
+        final var consideredClassName = ci.name();
         if (Modifier.isAbstract(ci.flags())) {
-            log.debugv("Skipping ''{0}'' controller because it's abstract", ci.name());
+            log.debugv("Skipping ''{0}'' controller because it's abstract", consideredClassName);
             return false;
         }
 
-        // Ignore ConfiguredController class
-        return !ci.name().equals(CONTROLLER);
+        // Ignore SDK internal Reconciler implementations
+        return !consideredClassName.equals(CONTROLLER) && !consideredClassName.equals(DEPENDENT_RESOURCE_MANAGER);
     }
 }
