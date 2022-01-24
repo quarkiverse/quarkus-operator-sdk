b73995b7c11f542b176475ed1f85852066a7a405
Chris Laprun <metacosm@gmail.com>
2022-01-24 11:18:13 +0100

    feat: update SDK to 2.1.0-SNAPSHOT, also ignore DependentResourceManager
    Whenever an internal SDK implements Reconciler, we need to ignore it so
    that it's not considered by the extension as user-provided.
    
    See java-operator-sdk/java-operator-sdk#862

--- common/src/main/java/io/quarkiverse/operatorsdk/common/Constants.java
+++ common/src/main/java/io/quarkiverse/operatorsdk/common/Constants.java
@@ -6,6 +6,7 @@ import io.fabric8.kubernetes.client.CustomResource;
 import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
 import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
 import io.javaoperatorsdk.operator.processing.Controller;
+import io.javaoperatorsdk.operator.processing.dependent.DependentResourceManager;
 
 public class Constants {
     private Constants() {
@@ -13,6 +14,8 @@ public class Constants {
 
     public static final DotName RECONCILER = DotName.createSimple(Reconciler.class.getName());
     public static final DotName CONTROLLER = DotName.createSimple(Controller.class.getName());
+    public static final DotName DEPENDENT_RESOURCE_MANAGER = DotName.createSimple(
+            DependentResourceManager.class.getName());
     public static final DotName CUSTOM_RESOURCE = DotName.createSimple(CustomResource.class.getName());
     public static final DotName CONTROLLER_CONFIGURATION = DotName.createSimple(ControllerConfiguration.class.getName());
 }
