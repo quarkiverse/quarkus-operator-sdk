be663017f0a61115b630ecacd5140d609fb32388
Chris Laprun <metacosm@gmail.com>
2022-01-25 15:27:28 +0100

    feat: skip `@Ignore` annotated reconcilers instead of hardcoding list

--- common/src/main/java/io/quarkiverse/operatorsdk/common/Constants.java
+++ common/src/main/java/io/quarkiverse/operatorsdk/common/Constants.java
@@ -4,18 +4,15 @@ import org.jboss.jandex.DotName;
 
 import io.fabric8.kubernetes.client.CustomResource;
 import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
+import io.javaoperatorsdk.operator.api.reconciler.Ignore;
 import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
-import io.javaoperatorsdk.operator.processing.Controller;
-import io.javaoperatorsdk.operator.processing.dependent.DependentResourceManager;
 
 public class Constants {
     private Constants() {
     }
 
     public static final DotName RECONCILER = DotName.createSimple(Reconciler.class.getName());
-    public static final DotName CONTROLLER = DotName.createSimple(Controller.class.getName());
-    public static final DotName DEPENDENT_RESOURCE_MANAGER = DotName.createSimple(
-            DependentResourceManager.class.getName());
+    public static final DotName IGNORE_RECONCILER = DotName.createSimple(Ignore.class.getName());
     public static final DotName CUSTOM_RESOURCE = DotName.createSimple(CustomResource.class.getName());
     public static final DotName CONTROLLER_CONFIGURATION = DotName.createSimple(ControllerConfiguration.class.getName());
 }
