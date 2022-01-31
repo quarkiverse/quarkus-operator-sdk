66b4eac64a9b21186460c4a56a0d315a2846f557
Chris Laprun <metacosm@users.noreply.github.com>
2022-01-31 23:01:03 +0100

    feat: initial support for dependent resources (#215)
    Fixes #210

--- common/src/main/java/io/quarkiverse/operatorsdk/common/Constants.java
+++ common/src/main/java/io/quarkiverse/operatorsdk/common/Constants.java
@@ -2,6 +2,7 @@ package io.quarkiverse.operatorsdk.common;
 
 import org.jboss.jandex.DotName;
 
+import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.fabric8.kubernetes.client.CustomResource;
 import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
 import io.javaoperatorsdk.operator.api.reconciler.Ignore;
@@ -14,5 +15,6 @@ public class Constants {
     public static final DotName RECONCILER = DotName.createSimple(Reconciler.class.getName());
     public static final DotName IGNORE_RECONCILER = DotName.createSimple(Ignore.class.getName());
     public static final DotName CUSTOM_RESOURCE = DotName.createSimple(CustomResource.class.getName());
+    public static final DotName HAS_METADATA = DotName.createSimple(HasMetadata.class.getName());
     public static final DotName CONTROLLER_CONFIGURATION = DotName.createSimple(ControllerConfiguration.class.getName());
 }
