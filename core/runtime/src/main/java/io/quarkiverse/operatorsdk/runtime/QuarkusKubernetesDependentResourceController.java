c0899e45c3debacab94050d985726353e298e5a4
Chris Laprun <metacosm@gmail.com>
2022-02-04 15:41:20 +0100

    chore(deps): update to JOSDK 2.2.0-SNAPSHOT

--- core/runtime/src/main/java/io/quarkiverse/operatorsdk/runtime/QuarkusKubernetesDependentResourceController.java
+++ core/runtime/src/main/java/io/quarkiverse/operatorsdk/runtime/QuarkusKubernetesDependentResourceController.java
@@ -1,9 +1,9 @@
 package io.quarkiverse.operatorsdk.runtime;
 
 import io.fabric8.kubernetes.api.model.HasMetadata;
-import io.javaoperatorsdk.operator.api.config.DependentResource;
-import io.javaoperatorsdk.operator.api.reconciler.dependent.KubernetesDependentResourceConfiguration;
-import io.javaoperatorsdk.operator.api.reconciler.dependent.KubernetesDependentResourceController;
+import io.javaoperatorsdk.operator.api.config.dependent.KubernetesDependentResourceConfiguration;
+import io.javaoperatorsdk.operator.api.reconciler.dependent.DependentResource;
+import io.javaoperatorsdk.operator.processing.dependent.KubernetesDependentResourceController;
 import io.quarkus.runtime.annotations.RecordableConstructor;
 
 public class QuarkusKubernetesDependentResourceController<R extends HasMetadata, P extends HasMetadata> extends
