2f68fb8a676d5ed87e2d0418810f6e8dcc28f8e9
Chris Laprun <metacosm@gmail.com>
2022-02-11 13:48:59 +0100

    fix: update after rebase

--- core/runtime/src/main/java/io/quarkiverse/operatorsdk/runtime/QuarkusKubernetesDependentResourceController.java
+++ core/runtime/src/main/java/io/quarkiverse/operatorsdk/runtime/QuarkusKubernetesDependentResourceController.java
@@ -2,7 +2,7 @@ package io.quarkiverse.operatorsdk.runtime;
 
 import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.javaoperatorsdk.operator.api.config.dependent.KubernetesDependentResourceConfiguration;
-import io.javaoperatorsdk.operator.api.reconciler.dependent.DependentResource;
+import io.javaoperatorsdk.operator.api.reconciler.dependent.KubernetesDependentResource;
 import io.javaoperatorsdk.operator.processing.dependent.KubernetesDependentResourceController;
 import io.quarkus.runtime.annotations.RecordableConstructor;
 
@@ -11,7 +11,7 @@ public class QuarkusKubernetesDependentResourceController<R extends HasMetadata,
 
     @RecordableConstructor
     public QuarkusKubernetesDependentResourceController(
-            DependentResource<R, P> delegate,
+            KubernetesDependentResource<R, P> delegate,
             KubernetesDependentResourceConfiguration<R, P> configuration) {
         super(delegate, configuration);
     }
