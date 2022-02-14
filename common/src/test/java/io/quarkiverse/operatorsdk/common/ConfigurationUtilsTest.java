b389b31dcbc660754dcc099aaff762be1c0a7651
Chris Laprun <metacosm@gmail.com>
2022-02-14 11:55:59 +0100

    feat!: remove deprecated configuration option

--- common/src/test/java/io/quarkiverse/operatorsdk/common/ConfigurationUtilsTest.java
+++ common/src/test/java/io/quarkiverse/operatorsdk/common/ConfigurationUtilsTest.java
@@ -1,8 +1,5 @@
 package io.quarkiverse.operatorsdk.common;
 
-import java.util.Optional;
-
-import org.jboss.logging.Logger;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -13,61 +10,7 @@ class ConfigurationUtilsTest {
     void shouldUseSystemPropertyIfPresent() {
         if (Utils.isValidateCustomResourcesEnvVarSet()) {
             Assertions.assertEquals(Utils.shouldCheckCRDAndValidateLocalModel(),
-                    ConfigurationUtils.shouldValidateCustomResources(Optional.empty(), false, null));
-        }
-    }
-
-    @Test
-    void shouldLogAWarningIfDeprecatedVersionIsUsed() {
-        checkLogging(true, false);
-        checkLogging(true, true);
-        checkLogging(false, false);
-        checkLogging(false, true);
-    }
-
-    private void checkLogging(boolean fromOld, boolean fromNew) {
-        var logger = new TestLogger("test", fromOld, fromNew);
-        ConfigurationUtils.shouldValidateCustomResources(Optional.of(fromOld), fromNew, logger);
-        Assertions.assertTrue(logger.wasCalled);
-    }
-
-    private static class TestLogger extends Logger {
-        private boolean wasCalled;
-        private boolean fromOld;
-        private boolean fromNew;
-
-        protected TestLogger(String name, boolean fromOld, boolean fromNew) {
-            super(name);
-            this.fromOld = fromOld;
-            this.fromNew = fromNew;
-        }
-
-        @Override
-        protected void doLog(Level level, String s, Object o, Object[] objects, Throwable throwable) {
-            Assertions.assertEquals(Level.WARN, level);
-            Assertions.assertTrue(o instanceof String);
-            var stringValue = (String) o;
-            if (fromOld != fromNew) {
-                Assertions.assertTrue(stringValue.contains("check-crd-and-validate-local-model with value '" + fromOld)
-                        && stringValue.contains("crd.validate property value '" + fromNew));
-            } else {
-                Assertions.assertTrue(stringValue.contains("deprecated")
-                        && !stringValue.contains("'" + fromOld)
-                        && !stringValue.contains("'" + fromNew));
-            }
-            wasCalled = true;
-        }
-
-        @Override
-        protected void doLogf(Level level, String s, String s1, Object[] objects, Throwable throwable) {
-            Assertions.assertEquals(Level.WARN, level);
-            wasCalled = true;
-            Assertions.assertTrue(s1.contains("deprecated"));
-        }
-
-        @Override
-        public boolean isEnabled(Level level) {
-            return true;
+                    ConfigurationUtils.shouldValidateCustomResources(false));
         }
     }
 }
--- core/runtime/src/main/java/io/quarkiverse/operatorsdk/runtime/BuildTimeOperatorConfiguration.java
+++ core/runtime/src/main/java/io/quarkiverse/operatorsdk/runtime/BuildTimeOperatorConfiguration.java
@@ -3,24 +3,12 @@ package io.quarkiverse.operatorsdk.runtime;
 import java.util.Map;
 import java.util.Optional;
 
-import io.fabric8.kubernetes.client.CustomResource;
 import io.quarkus.runtime.annotations.ConfigItem;
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
 
 @ConfigRoot(name = "operator-sdk", phase = ConfigPhase.BUILD_AND_RUN_TIME_FIXED)
 public class BuildTimeOperatorConfiguration {
-
-    /**
-     * Whether the operator should check that the CRD is properly deployed and that the associated
-     * {@link CustomResource} implementation matches its information before registering the associated
-     * controller.
-     *
-     * @deprecated Use {@link CRDConfiguration#validate} instead
-     */
-    @ConfigItem
-    @Deprecated(forRemoval = true)
-    public Optional<Boolean> checkCRDAndValidateLocalModel;
 
     /**
      * Maps a controller name to its configuration.
