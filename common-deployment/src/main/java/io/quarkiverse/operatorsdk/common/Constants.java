c7adff95e37aafc508da0bb5fb8679358929c4af
Chris Laprun <metacosm@gmail.com>
2022-02-28 21:10:25 +0100

    fix: initial support for new dependent resource architecture

--- common-deployment/src/main/java/io/quarkiverse/operatorsdk/common/Constants.java
+++ common-deployment/src/main/java/io/quarkiverse/operatorsdk/common/Constants.java
@@ -7,6 +7,8 @@ import io.fabric8.kubernetes.client.CustomResource;
 import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
 import io.javaoperatorsdk.operator.api.reconciler.Ignore;
 import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
+import io.javaoperatorsdk.operator.processing.dependent.kubernetes.KubernetesDependent;
+import io.javaoperatorsdk.operator.processing.dependent.kubernetes.KubernetesDependentResource;
 
 public class Constants {
     private Constants() {
@@ -17,4 +19,7 @@ public class Constants {
     public static final DotName CUSTOM_RESOURCE = DotName.createSimple(CustomResource.class.getName());
     public static final DotName HAS_METADATA = DotName.createSimple(HasMetadata.class.getName());
     public static final DotName CONTROLLER_CONFIGURATION = DotName.createSimple(ControllerConfiguration.class.getName());
+    public static final DotName KUBERNETES_DEPENDENT_RESOURCE = DotName
+            .createSimple(KubernetesDependentResource.class.getName());
+    public static final DotName KUBERNETES_DEPENDENT = DotName.createSimple(KubernetesDependent.class.getName());
 }
