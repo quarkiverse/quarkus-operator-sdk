bca4f1778838d9c209b87ad7126910572c8fe6f0
Chris Laprun <metacosm@gmail.com>
2022-03-01 09:55:10 +0100

    fix: improper logging

--- csv-generator/deployment/src/main/java/io/quarkiverse/operatorsdk/csv/deployment/CSVProcessor.java
+++ csv-generator/deployment/src/main/java/io/quarkiverse/operatorsdk/csv/deployment/CSVProcessor.java
@@ -56,7 +56,7 @@ public class CSVProcessor {
                 .forEach(reconcilerInfo -> {
                     // figure out which group should be used to generate CSV
                     final var name = reconcilerInfo.name();
-                    log.infov("Processing reconciler: {}", name);
+                    log.debugv("Processing reconciler: {0}", name);
                     final var config = controllerConfigs.get(name);
                     if (config == null) {
                         throw new IllegalStateException("Missing configuration for reconciler " + name);
