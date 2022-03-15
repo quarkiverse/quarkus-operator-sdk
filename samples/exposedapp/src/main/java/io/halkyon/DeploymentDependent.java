c6f4bbb15a6172dcd1bb024d7639657d5e56a064
Chris Laprun <metacosm@users.noreply.github.com>
2022-03-15 22:17:37 +0100

    feat: add ExposedApp sample (#274)
    * feat: add ExposedApp sample
    
    * fix: format
    
    * fix: ignore NPEs when the server is not returning the objects initially
    
    * fix: remove Java 17 requirement
    
    * fix: add Quarkus maven plugin configuration and native profile

---
+++ samples/exposedapp/src/main/java/io/halkyon/DeploymentDependent.java
@@ -0,0 +1,75 @@
+package io.halkyon;
+
+import static io.halkyon.ExposedAppReconciler.LABELS_CONTEXT_KEY;
+import static io.halkyon.ExposedAppReconciler.createMetadata;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import io.fabric8.kubernetes.api.model.EnvVar;
+import io.fabric8.kubernetes.api.model.apps.Deployment;
+import io.fabric8.kubernetes.api.model.apps.DeploymentBuilder;
+import io.javaoperatorsdk.operator.api.reconciler.Context;
+import io.javaoperatorsdk.operator.api.reconciler.dependent.Creator;
+import io.javaoperatorsdk.operator.api.reconciler.dependent.Matcher;
+import io.javaoperatorsdk.operator.processing.dependent.kubernetes.KubernetesDependentResource;
+
+public class DeploymentDependent extends
+        KubernetesDependentResource<Deployment, ExposedApp> implements
+        Creator<Deployment, ExposedApp>, Matcher<Deployment, ExposedApp> {
+
+    @SuppressWarnings("unchecked")
+    public Deployment desired(ExposedApp exposedApp, Context context) {
+        final var labels = (Map<String, String>) context.managedDependentResourceContext()
+                .getMandatory(LABELS_CONTEXT_KEY, Map.class);
+        final var name = exposedApp.getMetadata().getName();
+        final var spec = exposedApp.getSpec();
+        final var imageRef = spec.getImageRef();
+        final var env = spec.getEnv();
+
+        var containerBuilder = new DeploymentBuilder()
+                .withMetadata(createMetadata(exposedApp, labels))
+                .withNewSpec()
+                .withNewSelector().withMatchLabels(labels).endSelector()
+                .withNewTemplate()
+                .withNewMetadata().withLabels(labels).endMetadata()
+                .withNewSpec()
+                .addNewContainer()
+                .withName(name).withImage(imageRef);
+
+        // add env variables
+        if (env != null) {
+            env.forEach((key, value) -> containerBuilder.addNewEnv()
+                    .withName(key.toUpperCase())
+                    .withValue(value)
+                    .endEnv());
+        }
+
+        return containerBuilder
+                .addNewPort()
+                .withName("http").withProtocol("TCP").withContainerPort(8080)
+                .endPort()
+                .endContainer()
+                .endSpec()
+                .endTemplate()
+                .endSpec()
+                .build();
+    }
+
+    @Override
+    public Result<Deployment> match(Deployment actual, ExposedApp primary, Context<ExposedApp> context) {
+        final var desiredSpec = primary.getSpec();
+        final var container = actual.getSpec().getTemplate().getSpec().getContainers()
+                .stream()
+                .findFirst();
+        return Result.nonComputed(container.map(c -> c.getImage().equals(desiredSpec.getImageRef())
+                && desiredSpec.getEnv().equals(convert(c.getEnv()))).orElse(false));
+    }
+
+    private Map<String, String> convert(List<EnvVar> envVars) {
+        final var result = new HashMap<String, String>(envVars.size());
+        envVars.forEach(e -> result.put(e.getName(), e.getValue()));
+        return result;
+    }
+}
---
+++ samples/exposedapp/src/main/java/io/halkyon/ServiceDependent.java
@@ -0,0 +1,36 @@
+package io.halkyon;
+
+import static io.halkyon.ExposedAppReconciler.LABELS_CONTEXT_KEY;
+import static io.halkyon.ExposedAppReconciler.createMetadata;
+
+import java.util.Map;
+
+import io.fabric8.kubernetes.api.model.Service;
+import io.fabric8.kubernetes.api.model.ServiceBuilder;
+import io.javaoperatorsdk.operator.api.reconciler.Context;
+import io.javaoperatorsdk.operator.api.reconciler.dependent.Creator;
+import io.javaoperatorsdk.operator.processing.dependent.kubernetes.KubernetesDependentResource;
+
+public class ServiceDependent extends KubernetesDependentResource<Service, ExposedApp> implements
+        Creator<Service, ExposedApp> {
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public Service desired(ExposedApp exposedApp, Context context) {
+        final var labels = (Map<String, String>) context.managedDependentResourceContext()
+                .getMandatory(LABELS_CONTEXT_KEY, Map.class);
+
+        return new ServiceBuilder()
+                .withMetadata(createMetadata(exposedApp, labels))
+                .withNewSpec()
+                .addNewPort()
+                .withName("http")
+                .withPort(8080)
+                .withNewTargetPort().withIntVal(8080).endTargetPort()
+                .endPort()
+                .withSelector(labels)
+                .withType("ClusterIP")
+                .endSpec()
+                .build();
+    }
+}
