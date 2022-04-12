c79ecbd70c6db2a95f44d458dcbeea64ac7e0b51
Chris Laprun <metacosm@gmail.com>
2022-04-12 21:41:56 +0200

    chore: check that all the files are indeed generated

--- bundle-generator/deployment/src/test/java/io/quarkiverse/operatorsdk/bundle/MultipleOperatorsBundleTest.java
+++ bundle-generator/deployment/src/test/java/io/quarkiverse/operatorsdk/bundle/MultipleOperatorsBundleTest.java
@@ -1,11 +1,13 @@
 package io.quarkiverse.operatorsdk.bundle;
 
 import java.nio.file.Files;
+import java.nio.file.Path;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
+import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.quarkiverse.operatorsdk.bundle.sources.First;
 import io.quarkiverse.operatorsdk.bundle.sources.FirstReconciler;
 import io.quarkiverse.operatorsdk.bundle.sources.Second;
@@ -29,8 +31,23 @@ public class MultipleOperatorsBundleTest {
 
     @Test
     public void shouldWriteBundleForTheTwoOperators() {
-        Assertions.assertTrue(Files.exists(prodModeTestResults.getBuildDir().resolve(BUNDLE).resolve("first-operator")));
-        Assertions.assertTrue(Files.exists(prodModeTestResults.getBuildDir().resolve(BUNDLE).resolve("second-operator")));
+        final var bundle = prodModeTestResults.getBuildDir().resolve(BUNDLE);
+        checkBundleFor(bundle, "first-operator", First.class);
+        checkBundleFor(bundle, "second-operator", Second.class);
+    }
+
+    private void checkBundleFor(Path bundle, String operatorName, Class<? extends HasMetadata> resourceClass) {
+        final var operatorManifests = bundle.resolve(operatorName);
+        Assertions.assertTrue(Files.exists(operatorManifests));
+        Assertions.assertTrue(Files.exists(operatorManifests.resolve("bundle.Dockerfile")));
+        final var manifests = operatorManifests.resolve("manifests");
+        Assertions.assertTrue(Files.exists(manifests));
+        Assertions.assertTrue(Files.exists(manifests.resolve(operatorName + ".csv.yml")));
+        Assertions.assertTrue(Files.exists(manifests.resolve(HasMetadata.getFullResourceName(
+                resourceClass) + "-v1.crd.yml")));
+        final var metadata = operatorManifests.resolve("metadata");
+        Assertions.assertTrue(Files.exists(metadata));
+        Assertions.assertTrue(Files.exists(metadata.resolve("annotations.yaml")));
     }
 
 }
