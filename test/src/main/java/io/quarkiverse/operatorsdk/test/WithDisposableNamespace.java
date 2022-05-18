dcc9c4f490cd3885dda3dc766fad2bfb6d3663fe
Chris Laprun <metacosm@gmail.com>
2022-05-18 16:43:39 +0200

    fix: restrict WithDisposableNamespace to annotated class

--- test/src/main/java/io/quarkiverse/operatorsdk/test/WithDisposableNamespace.java
+++ test/src/main/java/io/quarkiverse/operatorsdk/test/WithDisposableNamespace.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 import io.quarkus.test.common.QuarkusTestResource;
 
-@QuarkusTestResource(DisposableNamespaceTestResource.class)
+@QuarkusTestResource(value = DisposableNamespaceTestResource.class, restrictToAnnotatedClass = true)
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface WithDisposableNamespace {
