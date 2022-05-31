1a51e79d80b55284249d25da3d281d245f3044f9
Chris Laprun <metacosm@gmail.com>
2022-05-31 18:21:54 +0200

    chore: remove unused classes

--- core/integration-tests/src/main/java/io/quarkiverse/operatorsdk/it/Delayed.java
+++
@@ -1,9 +0,0 @@
-package io.quarkiverse.operatorsdk.it;
-
-import io.fabric8.kubernetes.model.annotation.Group;
-import io.fabric8.kubernetes.model.annotation.Version;
-
-@Group("josdk.quarkiverse.io")
-@Version("v1alpha1")
-public class Delayed extends TestResource {
-}
--- core/integration-tests/src/main/java/io/quarkiverse/operatorsdk/it/RegistrableReconciler.java
+++
@@ -1,9 +0,0 @@
-package io.quarkiverse.operatorsdk.it;
-
-import io.fabric8.kubernetes.api.model.HasMetadata;
-import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
-
-public interface RegistrableReconciler<R extends HasMetadata> extends Reconciler<R> {
-
-    boolean isInitialized();
-}
