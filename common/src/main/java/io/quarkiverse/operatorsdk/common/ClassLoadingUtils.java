e76d925aac09d6ddef0c1e94fec39afcf86978d9
Chris Laprun <metacosm@gmail.com>
2022-07-15 14:42:25 +0200

    feat: add instantiate method

--- common/src/main/java/io/quarkiverse/operatorsdk/common/ClassLoadingUtils.java
+++ common/src/main/java/io/quarkiverse/operatorsdk/common/ClassLoadingUtils.java
@@ -1,5 +1,7 @@
 package io.quarkiverse.operatorsdk.common;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class ClassLoadingUtils {
 
     private ClassLoadingUtils() {
@@ -11,6 +13,15 @@ public class ClassLoadingUtils {
             return (Class<T>) Thread.currentThread().getContextClassLoader().loadClass(className);
         } catch (Exception e) {
             throw new IllegalArgumentException("Couldn't find class " + className, e);
+        }
+    }
+
+    public static <T> T instantiate(Class<T> toInstantiate) {
+        try {
+            return toInstantiate.getConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            throw new IllegalArgumentException("Couldn't instantiate " + toInstantiate.getName(),
+                    e);
         }
     }
 }
