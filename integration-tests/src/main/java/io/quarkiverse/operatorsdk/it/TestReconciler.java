4c37d67cc01874e144293651b884c4bda0036ae9
Chris Laprun <metacosm@gmail.com>
2022-07-15 14:45:10 +0200

    fix: add support for event filters and max reconciliation interval
    Fixes #370

--- integration-tests/src/main/java/io/quarkiverse/operatorsdk/it/TestReconciler.java
+++ integration-tests/src/main/java/io/quarkiverse/operatorsdk/it/TestReconciler.java
@@ -1,8 +1,12 @@
 package io.quarkiverse.operatorsdk.it;
 
-import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
+import java.util.concurrent.TimeUnit;
 
-@ControllerConfiguration(name = TestReconciler.NAME)
+import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
+import io.javaoperatorsdk.operator.api.reconciler.ReconciliationMaxInterval;
+
+@ControllerConfiguration(name = TestReconciler.NAME, reconciliationMaxInterval = @ReconciliationMaxInterval(interval = TestReconciler.INTERVAL, timeUnit = TimeUnit.SECONDS))
 public class TestReconciler extends AbstractReconciler<Test> {
     public static final String NAME = "test";
+    public static final int INTERVAL = 50;
 }
