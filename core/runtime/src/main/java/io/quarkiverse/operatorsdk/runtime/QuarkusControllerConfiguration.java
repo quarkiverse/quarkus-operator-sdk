511670a9eef681c350bd8412f0ffa9219fe235a9
Chris Laprun <metacosm@gmail.com>
2022-07-15 14:45:10 +0200

    fix: add support for deprecated filters and max reconciliation interval
    Fixes #370

--- core/runtime/src/main/java/io/quarkiverse/operatorsdk/runtime/QuarkusControllerConfiguration.java
+++ core/runtime/src/main/java/io/quarkiverse/operatorsdk/runtime/QuarkusControllerConfiguration.java
@@ -1,7 +1,9 @@
 package io.quarkiverse.operatorsdk.runtime;
 
+import java.time.Duration;
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 import io.fabric8.kubernetes.api.model.HasMetadata;
@@ -10,11 +12,29 @@ import io.javaoperatorsdk.operator.api.config.ControllerConfiguration;
 import io.javaoperatorsdk.operator.api.config.RetryConfiguration;
 import io.javaoperatorsdk.operator.api.config.dependent.DependentResourceSpec;
 import io.javaoperatorsdk.operator.api.reconciler.Constants;
+import io.javaoperatorsdk.operator.processing.Controller;
+import io.javaoperatorsdk.operator.processing.event.rate.RateLimiter;
+import io.javaoperatorsdk.operator.processing.event.source.controller.ResourceEventFilter;
+import io.javaoperatorsdk.operator.processing.retry.Retry;
 import io.quarkus.runtime.annotations.IgnoreProperty;
 import io.quarkus.runtime.annotations.RecordableConstructor;
 
 @SuppressWarnings("rawtypes")
 public class QuarkusControllerConfiguration<R extends HasMetadata> implements ControllerConfiguration<R> {
+
+    // we need to create this class because Quarkus cannot reference the default implementation that
+    // JOSDK provides as it doesn't like lambdas at build time. The class also needs to be public
+    // because otherwise Quarkus isn't able to access it… :(
+    public final static class PassthroughResourceEventFilter implements ResourceEventFilter {
+
+        @Override
+        public boolean acceptChange(Controller controller, HasMetadata hasMetadata,
+                HasMetadata p1) {
+            return false;
+        }
+    }
+
+    private final static ResourceEventFilter DEFAULT = new PassthroughResourceEventFilter();
 
     private final String associatedReconcilerClassName;
     private final String name;
@@ -24,12 +44,15 @@ public class QuarkusControllerConfiguration<R extends HasMetadata> implements Co
     private final boolean statusPresentAndNotVoid;
     private final List<DependentResourceSpec> dependentResources;
     private final Class<R> resourceClass;
+    private final ResourceEventFilter<R> eventFilter;
+    private final Optional<Duration> maxReconciliationInterval;
     private String finalizer;
     private Set<String> namespaces;
     private RetryConfiguration retryConfiguration;
     private String labelSelector;
 
     @RecordableConstructor
+    @SuppressWarnings("unchecked")
     public QuarkusControllerConfiguration(
             String associatedReconcilerClassName,
             String name,
@@ -37,7 +60,8 @@ public class QuarkusControllerConfiguration<R extends HasMetadata> implements Co
             String crVersion, boolean generationAware,
             Class<R> resourceClass, Set<String> namespaces, String finalizerName, String labelSelector,
             boolean statusPresentAndNotVoid,
-            List<DependentResourceSpec> dependentResources) {
+            List<DependentResourceSpec> dependentResources, ResourceEventFilter<R> eventFilter,
+            Duration maxReconciliationInterval) {
         this.associatedReconcilerClassName = associatedReconcilerClassName;
         this.name = name;
         this.resourceTypeName = resourceTypeName;
@@ -50,6 +74,9 @@ public class QuarkusControllerConfiguration<R extends HasMetadata> implements Co
         this.labelSelector = labelSelector;
         this.statusPresentAndNotVoid = statusPresentAndNotVoid;
         this.dependentResources = dependentResources;
+        this.eventFilter = eventFilter != null ? eventFilter : DEFAULT;
+        this.maxReconciliationInterval = maxReconciliationInterval != null ? Optional.of(maxReconciliationInterval)
+                : ControllerConfiguration.super.maxReconciliationInterval();
     }
 
     @Override
@@ -135,5 +162,29 @@ public class QuarkusControllerConfiguration<R extends HasMetadata> implements Co
     @Override
     public List<DependentResourceSpec> getDependentResources() {
         return dependentResources;
+    }
+
+    @Override
+    public Retry getRetry() {
+        return ControllerConfiguration.super.getRetry();
+    }
+
+    @Override
+    public RateLimiter getRateLimiter() {
+        return ControllerConfiguration.super.getRateLimiter();
+    }
+
+    @Override
+    public ResourceEventFilter<R> getEventFilter() {
+        return eventFilter;
+    }
+
+    public Optional<Duration> maxReconciliationInterval() {
+        return maxReconciliationInterval;
+    }
+
+    // for Quarkus' RecordableConstructor
+    public Duration getMaxReconciliationInterval() {
+        return maxReconciliationInterval.orElseThrow();
     }
 }
--- integration-tests/src/main/java/io/quarkiverse/operatorsdk/it/TestReconciler.java
+++ integration-tests/src/main/java/io/quarkiverse/operatorsdk/it/TestReconciler.java
@@ -1,13 +1,17 @@
 package io.quarkiverse.operatorsdk.it;
 
+import java.util.concurrent.TimeUnit;
+
 import io.javaoperatorsdk.operator.api.reconciler.Context;
 import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
+import io.javaoperatorsdk.operator.api.reconciler.MaxReconciliationInterval;
 import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
 import io.javaoperatorsdk.operator.api.reconciler.UpdateControl;
 
-@ControllerConfiguration(name = TestReconciler.NAME)
+@ControllerConfiguration(name = TestReconciler.NAME, maxReconciliationInterval = @MaxReconciliationInterval(interval = TestReconciler.INTERVAL, timeUnit = TimeUnit.SECONDS))
 public class TestReconciler implements Reconciler<Test> {
     public static final String NAME = "test";
+    public static final int INTERVAL = 50;
 
     @Override
     public UpdateControl<Test> reconcile(Test test, Context<Test> context) throws Exception {
