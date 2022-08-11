8bf668b084cf4a20d67f694c55752616e94696d5
Chris Laprun <metacosm@gmail.com>
2022-08-11 09:07:26 +0200

    fix: properly create QuarkusDependentResourceSpec

--- core/runtime/src/main/java/io/quarkiverse/operatorsdk/runtime/QuarkusDependentResourceSpec.java
+++ core/runtime/src/main/java/io/quarkiverse/operatorsdk/runtime/QuarkusDependentResourceSpec.java
@@ -1,16 +1,26 @@
 package io.quarkiverse.operatorsdk.runtime;
 
+import java.util.Set;
+
 import io.javaoperatorsdk.operator.api.config.dependent.DependentResourceSpec;
 import io.javaoperatorsdk.operator.api.reconciler.dependent.DependentResource;
+import io.javaoperatorsdk.operator.processing.dependent.workflow.Condition;
 import io.quarkus.runtime.annotations.RecordableConstructor;
 
 public class QuarkusDependentResourceSpec<T extends DependentResource<?, ?>, C> extends DependentResourceSpec<T, C> {
 
     @RecordableConstructor
-    public QuarkusDependentResourceSpec(Class<T> dependentResourceClass, C dependentResourceConfig, String name) {
-        super(dependentResourceClass, dependentResourceConfig, name);
+    public QuarkusDependentResourceSpec(Class<T> dependentResourceClass, C dependentResourceConfig,
+            String name, Set<String> dependsOn,
+            Condition<?, ?> readyCondition,
+            Condition<?, ?> reconcileCondition,
+            Condition<?, ?> deletePostCondition) {
+        super(dependentResourceClass, dependentResourceConfig, name, dependsOn, readyCondition,
+                reconcileCondition, deletePostCondition);
     }
 
+    // For Quarkus RecordableConstructor
+    @SuppressWarnings("unused")
     public C getDependentResourceConfig() {
         return super.getDependentResourceConfiguration().orElse(null);
     }
