d516cd9f047269c25cc676383f681c75689bc6e5
Chris Laprun <metacosm@gmail.com>
2023-01-05 18:16:50 +0100

    chore: add Keycloak namespace override test

--- integration-tests/src/test/java/io/quarkiverse/operatorsdk/it/CustomKubernetesServerTestResource.java
+++ integration-tests/src/test/java/io/quarkiverse/operatorsdk/it/CustomKubernetesServerTestResource.java
@@ -4,6 +4,7 @@ import static io.quarkiverse.operatorsdk.it.EmptyReconciler.FROM_ENV_NS1;
 import static io.quarkiverse.operatorsdk.it.EmptyReconciler.FROM_ENV_NS2;
 import static io.quarkiverse.operatorsdk.it.NamespaceFromEnvReconciler.FROM_ENV_VAR_NS;
 
+import io.javaoperatorsdk.operator.ReconcilerUtils;
 import io.quarkus.test.kubernetes.client.KubernetesServerTestResource;
 
 public class CustomKubernetesServerTestResource extends KubernetesServerTestResource {
@@ -17,6 +18,8 @@ public class CustomKubernetesServerTestResource extends KubernetesServerTestReso
         System.setProperty("QUARKUS_OPERATOR_SDK_CONTROLLERS_" + EmptyReconciler.NAME.toUpperCase()
                 + "_NAMESPACES", FROM_ENV_NS1 + ",   " + FROM_ENV_NS2);
         System.setProperty(VariableNSReconciler.ENV_VAR_NAME, VariableNSReconciler.EXPECTED_NS_VALUE);
+        System.setProperty("QUARKUS_OPERATOR_SDK_CONTROLLERS_" + ReconcilerUtils.getDefaultNameFor(
+                KeycloakController.class).toUpperCase() + "_NAMESPACES", KeycloakController.FROM_ENV);
 
         server.expect().get().withPath("/version")
                 .andReturn(200, "{\"major\": \"13\", \"minor\": \"37\"}").always();
