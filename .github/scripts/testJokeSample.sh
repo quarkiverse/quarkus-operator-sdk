3ff63fb32e3388fd41ac49cb9b41f8242d8054cb
Jose <josecarvajalhilario@gmail.com>
2023-03-21 08:17:50 +0100

    fix: improve and fix OLM-related CI
    - make sure that failures do fail the build
    - improve logging
    - update to more recent OLM to fix issue with KinD

--- .github/scripts/testJokeSample.sh
+++ .github/scripts/testJokeSample.sh
@@ -1,7 +1,17 @@
+#!/usr/bin/env bash
 NAMESPACE="${1}"
+
+# Ensure the CRD is installed
+if ! .github/scripts/waitFor.sh crd $NAMESPACE jokerequests.samples.javaoperatorsdk.io; then
+  exit 1;
+fi
 
 # Test operator by creating a Joke Request resource
 kubectl apply -f samples/joke/src/main/k8s/jokerequest.yml
 
 # And wait for the operator to create another Joke resource
-.github/scripts/waitFor.sh joke $NAMESPACE NAME
+if ! .github/scripts/waitFor.sh joke $NAMESPACE NAME; then
+  exit 1;
+fi
+
+exit 0;
