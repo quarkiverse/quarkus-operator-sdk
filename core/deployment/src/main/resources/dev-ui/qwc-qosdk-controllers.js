255b7f193cd82f72efb155e98be214af63d492cc
Chris Laprun <metacosm@gmail.com>
2023-04-20 15:43:45 +0200

    feat: use list instead of table

--- core/deployment/src/main/resources/dev-ui/qwc-qosdk-controllers.js
+++ core/deployment/src/main/resources/dev-ui/qwc-qosdk-controllers.js
@@ -1,65 +1,98 @@
-import { LitElement, html, css, nothing} from 'lit';
-import { JsonRpc } from 'jsonrpc';
-import '@vaadin/grid';
+import {LitElement, html, nothing} from 'lit';
+import {JsonRpc} from 'jsonrpc';
 import '@vaadin/details';
 import '@vaadin/list-box';
 import '@vaadin/item';
-import {columnBodyRenderer} from '@vaadin/grid/lit.js';
+import '@vaadin/horizontal-layout';
+import 'qui-badge';
+
 export class QWCQOSDKControllers extends LitElement {
 
-    jsonRpc = new JsonRpc(this);
+  jsonRpc = new JsonRpc(this);
 
-    constructor() {
-        super();
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.jsonRpc.getControllers().then(
+        jsonRpcResponse => this._controllers = jsonRpcResponse.result);
+  }
+
+  static properties = {
+    _controllers: {state: true},
+  }
+
+  render() {
+    if (this._controllers) {
+      return html`
+        <vaadin-list-box>
+          ${this._controllers.map(this._renderController, this)}
+        </vaadin-list-box>
+      `
     }
+  }
 
-    render() {
-        return html`
-            <vaadin-grid .dataProvider="${this._controllersProvider}">
-                <vaadin-grid-column path="name"></vaadin-grid-column>
-                <vaadin-grid-column path="resourceClass"></vaadin-grid-column>
-                <vaadin-grid-column path="effectiveNamespaces"></vaadin-grid-column>
-                <vaadin-grid-column path="eventSources" auto-width ${columnBodyRenderer(this._eventSourcesRenderer, [])}></vaadin-grid-column>
-                <vaadin-grid-column path="dependents" auto-width
-                                    ${columnBodyRenderer(
-                                        this._dependentsRenderer, [])}></vaadin-grid-column>
-            </vaadin-grid>
-            `;
+  _renderController(controller) {
+    return html`
+      <vaadin-item>
+        ${resourceClassRenderer(controller.resourceClass)}
+        <vaadin-details theme="filled" summary="${controller.name}">
+          <vaadin-horizontal-layout theme="spacing padding">
+            ${this.childrenRenderer(controller.dependents, "Dependents",
+                this.dependentRenderer)}
+            ${this.childrenRenderer(controller.eventSources, "Event Sources",
+                this.eventSourceRenderer)}
+          </vaadin-horizontal-layout>
+        </vaadin-details>
+      </vaadin-item>`
+  }
+
+  eventSourceRenderer(eventSource) {
+    return html`
+      <qui-badge>${eventSource.name}</qui-badge>
+      ${resourceClassRenderer(eventSource.resourceClass)}
+    `
+  }
+
+  dependentRenderer(dependent) {
+    let defaultName = dependent.name === dependent.type;
+    return html`
+      <qui-badge>${dependent.name}</qui-badge>
+      ${defaultName ? '' : resourceClassRenderer(dependent.type)}`
+  }
+
+  childrenRenderer = (children, childrenName, childRenderer) => {
+    if (children) {
+      let count = children.length;
+      return html`
+        <vaadin-details theme="filled" summary="${count} ${childrenName}">
+          <vaadin-list-box>
+            ${children.map((child) => html`
+              <vaadin-item>
+                ${childRenderer(child)}
+              </vaadin-item>
+            `)}
+          </vaadin-list-box>
+        </vaadin-details>
+      `
     }
+  }
 
-    _controllersProvider = (params, callback) => {
-        let items = [];
-        this.jsonRpc.getControllers().then(jsonRpcResponse => {
-            console.log(jsonRpcResponse);
-            callback(jsonRpcResponse.result, jsonRpcResponse.result.length);
-        })
-    }
-
-    _eventSourcesRenderer = (controller) => {
-       return html`
-           <vaadin-list-box>
-               ${controller.eventSources.map((eventSource) => html`
-                   <vaadin-item>
-                       ${eventSource.name}
-                       ${eventSource.resourceClass ?? nothing}
-                   </vaadin-item>
-               `)}
-           </vaadin-list-box>
-       `
-    };
-
-    _dependentsRenderer = (controller) => {
-        return html`
-            <vaadin-list-box>
-                ${controller.dependents.map((dependent) => html`
-                   <vaadin-item>
-                       ${dependent.name}
-                       ${dependent.type}
-                   </vaadin-item>
-               `)}
-            </vaadin-list-box>
-        `
-    };
-    
 }
+
+function resourceClassRenderer(resourceClass) {
+  if (resourceClass) {
+    const fabric8Prefix = 'io.fabric8.kubernetes.api.model.';
+    let level = 'success';
+    if (resourceClass.startsWith(fabric8Prefix)) {
+      level = 'warning';
+      resourceClass = resourceClass.substring(fabric8Prefix.length);
+    }
+    return html`
+      <qui-badge level="${level}" small>${resourceClass}</qui-badge>`
+  }
+}
+
 customElements.define('qwc-qosdk-controllers', QWCQOSDKControllers);
