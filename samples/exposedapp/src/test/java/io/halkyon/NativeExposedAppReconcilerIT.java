d490738aa9a1b635cfa6e70d79208805a898b625
Chris Laprun <metacosm@gmail.com>
2023-05-04 09:25:22 +0200

    fix: disable ExposedApp test in native mode for now

--- samples/exposedapp/src/test/java/io/halkyon/NativeExposedAppReconcilerIT.java
+++ samples/exposedapp/src/test/java/io/halkyon/NativeExposedAppReconcilerIT.java
@@ -1,8 +1,10 @@
 package io.halkyon;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import org.junit.jupiter.api.Disabled;
 
 @QuarkusIntegrationTest
+@Disabled("Currently not possible to inject the dev service-provided k8s client in native app")
 public class NativeExposedAppReconcilerIT extends ExposedAppReconcilerTest {
 
 }
