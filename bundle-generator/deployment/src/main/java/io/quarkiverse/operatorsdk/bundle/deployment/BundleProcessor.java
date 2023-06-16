3f969f2cee06bab1f455a7abc500dc7ccb0976c6
Chris Laprun <metacosm@gmail.com>
2023-06-16 14:11:38 +0200

    fix: an exception during bundle generation should fail the build

--- bundle-generator/deployment/src/main/java/io/quarkiverse/operatorsdk/bundle/deployment/BundleProcessor.java
+++ bundle-generator/deployment/src/main/java/io/quarkiverse/operatorsdk/bundle/deployment/BundleProcessor.java
@@ -178,80 +178,77 @@ public class BundleProcessor {
             List<GeneratedKubernetesResourceBuildItem> generatedKubernetesManifests,
             BuildProducer<GeneratedFileSystemResourceBuildItem> generatedCSVs) {
         if (bundleConfiguration.enabled) {
-            try {
-                final var crds = generatedCustomResourcesDefinitions.getCRDGenerationInfo().getCrds()
-                        .values().stream()
-                        .flatMap(entry -> entry.values().stream())
-                        .collect(Collectors.toMap(CRDInfo::getCrdName, Function.identity()));
-                final var outputDir = outputTarget.getOutputDirectory().resolve(BUNDLE);
-                final var serviceAccounts = new LinkedList<ServiceAccount>();
-                final var clusterRoleBindings = new LinkedList<ClusterRoleBinding>();
-                final var clusterRoles = new LinkedList<ClusterRole>();
-                final var roleBindings = new LinkedList<RoleBinding>();
-                final var roles = new LinkedList<Role>();
-                final var deployments = new LinkedList<Deployment>();
+            final var crds = generatedCustomResourcesDefinitions.getCRDGenerationInfo().getCrds()
+                    .values().stream()
+                    .flatMap(entry -> entry.values().stream())
+                    .collect(Collectors.toMap(CRDInfo::getCrdName, Function.identity()));
+            final var outputDir = outputTarget.getOutputDirectory().resolve(BUNDLE);
+            final var serviceAccounts = new LinkedList<ServiceAccount>();
+            final var clusterRoleBindings = new LinkedList<ClusterRoleBinding>();
+            final var clusterRoles = new LinkedList<ClusterRole>();
+            final var roleBindings = new LinkedList<RoleBinding>();
+            final var roles = new LinkedList<Role>();
+            final var deployments = new LinkedList<Deployment>();
 
-                generatedKubernetesManifests.stream()
-                        .filter(bi -> bi.getName().equals("kubernetes.yml"))
-                        .findAny()
-                        .ifPresent(
-                                bi -> {
-                                    final var resources = Serialization
-                                            .unmarshalAsList(new ByteArrayInputStream(bi.getContent()));
-                                    resources.getItems().forEach(r -> {
-                                        if (r instanceof ServiceAccount) {
-                                            serviceAccounts.add((ServiceAccount) r);
-                                            return;
-                                        }
+            generatedKubernetesManifests.stream()
+                    .filter(bi -> bi.getName().equals("kubernetes.yml"))
+                    .findAny()
+                    .ifPresent(
+                            bi -> {
+                                final var resources = Serialization
+                                        .unmarshalAsList(new ByteArrayInputStream(bi.getContent()));
+                                resources.getItems().forEach(r -> {
+                                    if (r instanceof ServiceAccount) {
+                                        serviceAccounts.add((ServiceAccount) r);
+                                        return;
+                                    }
 
-                                        if (r instanceof ClusterRoleBinding) {
-                                            clusterRoleBindings.add((ClusterRoleBinding) r);
-                                            return;
-                                        }
+                                    if (r instanceof ClusterRoleBinding) {
+                                        clusterRoleBindings.add((ClusterRoleBinding) r);
+                                        return;
+                                    }
 
-                                        if (r instanceof ClusterRole) {
-                                            clusterRoles.add((ClusterRole) r);
-                                            return;
-                                        }
+                                    if (r instanceof ClusterRole) {
+                                        clusterRoles.add((ClusterRole) r);
+                                        return;
+                                    }
 
-                                        if (r instanceof RoleBinding) {
-                                            roleBindings.add((RoleBinding) r);
-                                            return;
-                                        }
+                                    if (r instanceof RoleBinding) {
+                                        roleBindings.add((RoleBinding) r);
+                                        return;
+                                    }
 
-                                        if (r instanceof Role) {
-                                            roles.add((Role) r);
-                                            return;
-                                        }
+                                    if (r instanceof Role) {
+                                        roles.add((Role) r);
+                                        return;
+                                    }
 
-                                        if (r instanceof Deployment) {
-                                            deployments.add((Deployment) r);
-                                        }
-                                    });
+                                    if (r instanceof Deployment) {
+                                        deployments.add((Deployment) r);
+                                    }
                                 });
-                final var generated = BundleGenerator.prepareGeneration(bundleConfiguration, versionBuildItem.getVersion(),
-                        csvMetadata.getCsvGroups(), crds, outputTarget.getOutputDirectory());
-                generated.forEach(manifestBuilder -> {
-                    final var fileName = manifestBuilder.getFileName();
-                    try {
-                        generatedCSVs.produce(
-                                new GeneratedFileSystemResourceBuildItem(
-                                        Path.of(BUNDLE).resolve(manifestBuilder.getName()).resolve(fileName).toString(),
-                                        manifestBuilder.getManifestData(serviceAccounts, clusterRoleBindings, clusterRoles,
-                                                roleBindings, roles, deployments)));
-                        log.infov("Generating {0} for ''{1}'' controller -> {2}",
-                                manifestBuilder.getManifestType(),
-                                manifestBuilder.getName(),
-                                outputDir.resolve(manifestBuilder.getName()).resolve(fileName));
-                    } catch (IOException e) {
-                        log.errorv("Cannot generate {0} for ''{1}'' controller: {2}",
-                                manifestBuilder.getManifestType(), manifestBuilder.getName(), e.getMessage());
-                    }
-                });
-                doneGeneratingCSV.produce(new GeneratedBundleBuildItem());
-            } catch (Exception e) {
-                log.infov(e, "Couldn't generate bundle:");
-            }
+                            });
+            final var generated = BundleGenerator.prepareGeneration(bundleConfiguration, versionBuildItem.getVersion(),
+                    csvMetadata.getCsvGroups(), crds, outputTarget.getOutputDirectory());
+            generated.forEach(manifestBuilder -> {
+                final var fileName = manifestBuilder.getFileName();
+                try {
+                    generatedCSVs.produce(
+                            new GeneratedFileSystemResourceBuildItem(
+                                    Path.of(BUNDLE).resolve(manifestBuilder.getName()).resolve(fileName).toString(),
+                                    manifestBuilder.getManifestData(serviceAccounts, clusterRoleBindings, clusterRoles,
+                                            roleBindings, roles, deployments)));
+                    log.infov("Generating {0} for ''{1}'' controller -> {2}",
+                            manifestBuilder.getManifestType(),
+                            manifestBuilder.getName(),
+                            outputDir.resolve(manifestBuilder.getName()).resolve(fileName));
+                } catch (IOException e) {
+                    log.errorv("Cannot generate {0} for ''{1}'' controller: {2}",
+                            manifestBuilder.getManifestType(), manifestBuilder.getName(), e.getMessage());
+                }
+            });
+            doneGeneratingCSV.produce(new GeneratedBundleBuildItem());
+
         }
     }
 
