fc6f2a21eafe1d84e1cc54d4845d9420e979d3a5
Chris Laprun <metacosm@gmail.com>
2023-06-27 09:05:25 +0200

    refactor: remove unneeded constants

--- common-deployment/src/main/java/io/quarkiverse/operatorsdk/common/Constants.java
+++ common-deployment/src/main/java/io/quarkiverse/operatorsdk/common/Constants.java
@@ -2,8 +2,6 @@ package io.quarkiverse.operatorsdk.common;
 
 import org.jboss.jandex.DotName;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.fabric8.kubernetes.client.CustomResource;
 import io.javaoperatorsdk.operator.api.config.AnnotationConfigurable;
@@ -12,7 +10,6 @@ import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
 import io.javaoperatorsdk.operator.api.reconciler.Ignore;
 import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
 import io.javaoperatorsdk.operator.api.reconciler.dependent.DependentResource;
-import io.quarkus.kubernetes.client.KubernetesClientObjectMapper;
 
 public class Constants {
     private Constants() {
@@ -27,6 +24,4 @@ public class Constants {
     public static final DotName CONFIGURED = DotName.createSimple(Configured.class.getName());
     public static final DotName ANNOTATION_CONFIGURABLE = DotName.createSimple(AnnotationConfigurable.class.getName());
     public static final DotName OBJECT = DotName.createSimple(Object.class.getName());
-    public static final DotName OBJECT_MAPPER = DotName.createSimple(ObjectMapper.class);
-    public static final DotName KUBERNETES_CLIENT_OBJECT_MAPPER = DotName.createSimple(KubernetesClientObjectMapper.class);
 }
