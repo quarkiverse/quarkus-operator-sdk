f049e2e96d4db5b4cdc0925399301bc3237eebfb
Chris Laprun <metacosm@gmail.com>
2023-08-17 16:46:39 +0200

    fix: bundle generation configuration shouldn't be available at runtime
    Fixes #689

--- bundle-generator/runtime/src/main/java/io/quarkiverse/operatorsdk/bundle/runtime/BundleGenerationConfiguration.java
+++ bundle-generator/runtime/src/main/java/io/quarkiverse/operatorsdk/bundle/runtime/BundleGenerationConfiguration.java
@@ -4,10 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import io.quarkus.runtime.annotations.ConfigItem;
-import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
 
-@ConfigRoot(name = "operator-sdk.bundle", phase = ConfigPhase.BUILD_AND_RUN_TIME_FIXED)
+@ConfigRoot(name = "operator-sdk.bundle")
 public class BundleGenerationConfiguration {
     /**
      * Whether the extension should generate the Operator bundle.
