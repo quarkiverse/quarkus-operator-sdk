f2ae45dd44d14028e9b96028e2b48e6740197a9c
Chris Laprun <claprun@redhat.com>
2023-11-20 16:58:22 +0100

    fix: make DefaultRateLimiter class discoverable for configuration
    Fixes #756
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>

--- integration-tests/src/main/java/io/quarkiverse/operatorsdk/it/ConfiguredReconciler.java
+++ integration-tests/src/main/java/io/quarkiverse/operatorsdk/it/ConfiguredReconciler.java
@@ -1,13 +1,17 @@
 package io.quarkiverse.operatorsdk.it;
 
+import java.util.concurrent.TimeUnit;
+
 import io.javaoperatorsdk.operator.api.reconciler.Context;
 import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
 import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
 import io.javaoperatorsdk.operator.api.reconciler.UpdateControl;
+import io.javaoperatorsdk.operator.processing.event.rate.RateLimited;
 import io.javaoperatorsdk.operator.processing.retry.GradualRetry;
 
 @ControllerConfiguration(name = ConfiguredReconciler.NAME, namespaces = "foo")
 @GradualRetry(maxAttempts = ConfiguredReconciler.MAX_ATTEMPTS)
+@RateLimited(maxReconciliations = 1, within = 1, unit = TimeUnit.MINUTES)
 public class ConfiguredReconciler implements Reconciler<ChildTestResource2> {
 
     public static final String NAME = "annotation";
