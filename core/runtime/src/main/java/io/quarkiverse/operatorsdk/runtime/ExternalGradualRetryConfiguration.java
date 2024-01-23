dd4670231ecaf951740355c48976635fb1632f78
Chris Laprun <claprun@redhat.com>
2024-01-23 10:40:13 +0100

    feat: update retry configuration support to only apply to GenericRetry
    Removed support for RetryConfiguration and clarified what the retry
    configuration actually supports. Also fixed an issue where the
    annotation was actually taking precedence over the properties.
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>

--- core/runtime/src/main/java/io/quarkiverse/operatorsdk/runtime/ExternalGradualRetryConfiguration.java
+++ core/runtime/src/main/java/io/quarkiverse/operatorsdk/runtime/ExternalGradualRetryConfiguration.java
@@ -5,8 +5,13 @@ import java.util.Optional;
 import io.quarkus.runtime.annotations.ConfigGroup;
 import io.quarkus.runtime.annotations.ConfigItem;
 
+/**
+ * Configuration for {@link io.javaoperatorsdk.operator.processing.retry.GradualRetry}. Will only apply if the
+ * {@link io.javaoperatorsdk.operator.processing.retry.Retry} implementation class is
+ * {@link io.javaoperatorsdk.operator.processing.retry.GenericRetry}.
+ */
 @ConfigGroup
-public class ExternalRetryConfiguration {
+public class ExternalGradualRetryConfiguration {
 
     /**
      * How many times an operation should be retried before giving up
@@ -18,5 +23,5 @@ public class ExternalRetryConfiguration {
      * The configuration of the retry interval.
      */
     @ConfigItem
-    public ExternalIntervalConfiguration interval;
+    public ExternalGradualRetryIntervalConfiguration interval;
 }
