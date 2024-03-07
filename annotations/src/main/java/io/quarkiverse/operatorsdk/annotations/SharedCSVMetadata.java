a1fc629ee77d43d7bdcf562914b3d2879305245b
Chris Laprun <claprun@redhat.com>
2024-03-07 17:15:01 +0100

    docs: add bundle metadata details, improve tests
    Signed-off-by: Chris Laprun <claprun@redhat.com>

--- annotations/src/main/java/io/quarkiverse/operatorsdk/annotations/SharedCSVMetadata.java
+++ annotations/src/main/java/io/quarkiverse/operatorsdk/annotations/SharedCSVMetadata.java
@@ -1,4 +1,8 @@
 package io.quarkiverse.operatorsdk.annotations;
 
+/**
+ * A marker interface used to identify classes bearing {@link CSVMetadata} that needs to be shared across reconcilers using the
+ * same {@link CSVMetadata#name()} attribute. Note that sharing metadata without using {@link SharedCSVMetadata} is not allowed.
+ */
 public interface SharedCSVMetadata {
 }
