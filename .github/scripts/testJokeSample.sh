bbae7f9c6571851fd13141b341f8b895d075eb8b
Chris Laprun <claprun@redhat.com>
2024-03-07 10:38:36 +0100

    refactor(ci): simplify
    Signed-off-by: Chris Laprun <claprun@redhat.com>

--- .github/scripts/testJokeSample.sh
+++ .github/scripts/testJokeSample.sh
@@ -3,6 +3,7 @@ NAMESPACE="${1}"
 
 # Ensure the CRD is installed
 if ! .github/scripts/waitFor.sh crd $NAMESPACE jokerequests.samples.javaoperatorsdk.io; then
+  echo "CRD not installed: cannot run the Joke operator"
   exit 1;
 fi
 
@@ -11,6 +12,7 @@ kubectl apply -f samples/joke/src/main/k8s/jokerequest.yml
 
 # And wait for the operator to create another Joke resource
 if ! .github/scripts/waitFor.sh joke $NAMESPACE NAME; then
+  echo "Joke was not properly created in response to request"
   exit 1;
 fi
 
