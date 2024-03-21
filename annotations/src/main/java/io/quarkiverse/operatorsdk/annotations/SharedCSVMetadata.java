f002555a15cb454f996187a7202484b84d9d8c2d
Chris Laprun <claprun@redhat.com>
2024-03-21 14:37:29 +0100

    refactor: use bundleName instead of deprecated name
    Signed-off-by: Chris Laprun <claprun@redhat.com>

--- annotations/src/main/java/io/quarkiverse/operatorsdk/annotations/SharedCSVMetadata.java
+++ annotations/src/main/java/io/quarkiverse/operatorsdk/annotations/SharedCSVMetadata.java
@@ -2,7 +2,8 @@ package io.quarkiverse.operatorsdk.annotations;
 
 /**
  * A marker interface used to identify classes bearing {@link CSVMetadata} that needs to be shared across reconcilers using the
- * same {@link CSVMetadata#name()} attribute. Note that sharing metadata without using {@link SharedCSVMetadata} is not allowed.
+ * same {@link CSVMetadata#bundleName()} attribute. Note that sharing metadata without using {@link SharedCSVMetadata} is not
+ * allowed.
  */
 public interface SharedCSVMetadata {
 }
--- samples/pingpong/src/main/java/io/quarkiverse/operatorsdk/samples/pingpong/PingPongOperatorCSVMetadata.java
+++ samples/pingpong/src/main/java/io/quarkiverse/operatorsdk/samples/pingpong/PingPongOperatorCSVMetadata.java
@@ -3,6 +3,7 @@ package io.quarkiverse.operatorsdk.samples.pingpong;
 import io.quarkiverse.operatorsdk.annotations.CSVMetadata;
 import io.quarkiverse.operatorsdk.annotations.SharedCSVMetadata;
 
-@CSVMetadata(name = "pingpong-operator")
+@CSVMetadata(bundleName = "pingpong-operator")
+@SuppressWarnings("unused")
 public class PingPongOperatorCSVMetadata implements SharedCSVMetadata {
 }
