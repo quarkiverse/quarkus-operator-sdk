953b406e8fabba5ba172941480ca41103defbbea
Chris Laprun <claprun@redhat.com>
2024-03-26 18:45:28 +0100

    feat: install a shutdown hook if termination timeout is set
    Signed-off-by: Chris Laprun <claprun@redhat.com>

--- core/runtime/src/main/java/io/quarkiverse/operatorsdk/runtime/RunTimeOperatorConfiguration.java
+++ core/runtime/src/main/java/io/quarkiverse/operatorsdk/runtime/RunTimeOperatorConfiguration.java
@@ -42,7 +42,10 @@ public class RunTimeOperatorConfiguration {
     public Optional<Integer> concurrentReconciliationThreads;
 
     /**
-     * Amount of seconds the SDK waits for reconciliation threads to terminate before shutting down.
+     * Amount of seconds the SDK waits for reconciliation threads to terminate before shutting down. Setting this value will
+     * install a shutdown hook to wait for termination (causing
+     * {@link io.javaoperatorsdk.operator.Operator#installShutdownHook(Duration)} to be called with
+     * `Duration.ofSeconds(terminationTimeoutSeconds)`).
      */
     @ConfigItem
     public Optional<Integer> terminationTimeoutSeconds;
