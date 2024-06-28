aaee34cbd5b23395e77885425484460e2efe7a84
Chris Laprun <claprun@redhat.com>
2024-06-28 14:44:58 +0200

    feat: adapt to informer configuration changes
    Signed-off-by: Chris Laprun <claprun@redhat.com>

--- bundle-generator/deployment/src/test/java/io/quarkiverse/operatorsdk/bundle/sources/SecondReconciler.java
+++ bundle-generator/deployment/src/test/java/io/quarkiverse/operatorsdk/bundle/sources/SecondReconciler.java
@@ -4,12 +4,13 @@ import io.javaoperatorsdk.operator.api.reconciler.Context;
 import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
 import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
 import io.javaoperatorsdk.operator.api.reconciler.UpdateControl;
+import io.javaoperatorsdk.operator.processing.dependent.kubernetes.InformerConfig;
 import io.quarkiverse.operatorsdk.annotations.CSVMetadata;
 import io.quarkiverse.operatorsdk.annotations.RBACRule;
 
 @CSVMetadata(bundleName = "second-operator")
 @RBACRule(apiGroups = SecondReconciler.RBAC_RULE_GROUP, resources = SecondReconciler.RBAC_RULE_RES, verbs = SecondReconciler.RBAC_RULE_VERBS)
-@ControllerConfiguration(namespaces = "foo")
+@ControllerConfiguration(informerConfig = @InformerConfig(namespaces = "foo"))
 public class SecondReconciler implements Reconciler<Second> {
     public static final String RBAC_RULE_GROUP = "halkyon.io";
     public static final String RBAC_RULE_RES = "SomeResource";
