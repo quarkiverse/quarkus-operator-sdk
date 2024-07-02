9747c38d12de5eeb25c0cb562ecfec896b14607b
Chris Laprun <claprun@redhat.com>
2024-07-02 10:57:05 +0200

    feat: configure max reconciliation interval via property
    Fixes #893
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>

--- core/runtime/src/main/java/io/quarkiverse/operatorsdk/runtime/RunTimeControllerConfiguration.java
+++ core/runtime/src/main/java/io/quarkiverse/operatorsdk/runtime/RunTimeControllerConfiguration.java
@@ -2,9 +2,11 @@ package io.quarkiverse.operatorsdk.runtime;
 
 import static io.quarkiverse.operatorsdk.runtime.Constants.QOSDK_USE_BUILDTIME_NAMESPACES;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Optional;
 
+import io.javaoperatorsdk.operator.api.reconciler.MaxReconciliationInterval;
 import io.quarkus.runtime.annotations.ConfigGroup;
 import io.quarkus.runtime.annotations.ConfigItem;
 
@@ -35,8 +37,17 @@ public class RunTimeControllerConfiguration {
 
     /**
      * An optional list of comma-separated label selectors that Custom Resources must match to trigger the controller.
-     * See https://kubernetes.io/docs/concepts/overview/working-with-objects/labels/ for more details on selectors.
+     * See <a href="https://kubernetes.io/docs/concepts/overview/working-with-objects/labels/">...</a> for more details on
+     * selectors.
      */
     @ConfigItem
     public Optional<String> selector;
+
+    /**
+     * An optional {@link Duration} to specify the maximum time that is allowed to elapse before a reconciliation will happen
+     * regardless of the presence of events. See {@link MaxReconciliationInterval#interval()} for more details.
+     * Value is specified according to the rules defined at {@link Duration#parse(CharSequence)}.
+     */
+    @ConfigItem
+    public Optional<Duration> maxReconciliationInterval;
 }
