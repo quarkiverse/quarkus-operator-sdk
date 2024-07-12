71e195f24758dfe6fe916c5f9f800514e2fb8de2
Chris Laprun <claprun@redhat.com>
2024-07-12 18:49:44 +0200

    refactor: rename based on JOSDK changes
    Signed-off-by: Chris Laprun <claprun@redhat.com>

--- integration-tests/src/main/java/io/quarkiverse/operatorsdk/it/CustomRateLimiterReconciler.java
+++ integration-tests/src/main/java/io/quarkiverse/operatorsdk/it/CustomRateLimiterReconciler.java
@@ -1,13 +1,13 @@
 package io.quarkiverse.operatorsdk.it;
 
 import io.fabric8.kubernetes.api.model.ResourceQuota;
+import io.javaoperatorsdk.operator.api.config.informer.Informer;
 import io.javaoperatorsdk.operator.api.reconciler.Context;
 import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
 import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
 import io.javaoperatorsdk.operator.api.reconciler.UpdateControl;
-import io.javaoperatorsdk.operator.processing.dependent.kubernetes.InformerConfig;
 
-@ControllerConfiguration(name = CustomRateLimiterReconciler.NAME, rateLimiter = CustomRateLimiter.class, informerConfig = @InformerConfig(itemStore = NullItemStore.class))
+@ControllerConfiguration(name = CustomRateLimiterReconciler.NAME, rateLimiter = CustomRateLimiter.class, informer = @Informer(itemStore = NullItemStore.class))
 @CustomRateConfiguration(42)
 public class CustomRateLimiterReconciler implements Reconciler<ResourceQuota> {
 
