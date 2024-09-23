74e3c38d6fd1ae4fc3146d8e9e35a2b595d4fc9a
Chris Laprun <claprun@redhat.com>
2024-09-23 21:42:58 +0200

    fix: update WatchAllReconciler configuration after rebase
    Signed-off-by: Chris Laprun <claprun@redhat.com>

--- core/deployment/src/test/java/io/quarkiverse/operatorsdk/test/sources/WatchAllReconciler.java
+++ core/deployment/src/test/java/io/quarkiverse/operatorsdk/test/sources/WatchAllReconciler.java
@@ -1,13 +1,14 @@
 package io.quarkiverse.operatorsdk.test.sources;
 
 import io.fabric8.kubernetes.api.model.ConfigMap;
+import io.javaoperatorsdk.operator.api.config.informer.Informer;
 import io.javaoperatorsdk.operator.api.reconciler.Constants;
 import io.javaoperatorsdk.operator.api.reconciler.Context;
 import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
 import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
 import io.javaoperatorsdk.operator.api.reconciler.UpdateControl;
 
-@ControllerConfiguration(name = WatchAllReconciler.NAME, namespaces = Constants.WATCH_ALL_NAMESPACES)
+@ControllerConfiguration(name = WatchAllReconciler.NAME, informer = @Informer(namespaces = Constants.WATCH_ALL_NAMESPACES))
 public class WatchAllReconciler implements Reconciler<ConfigMap> {
 
     public static final String NAME = "watchall";
