d81d5035fb401df27d3e5d6bfb3f67cfa9b2ff19
Chris Laprun <claprun@redhat.com>
2024-11-14 14:16:23 +0100

    fix: use proper workflow definition
    Signed-off-by: Chris Laprun <claprun@redhat.com>

--- bundle-generator/deployment/src/test/java/io/quarkiverse/operatorsdk/bundle/sources/ReconcilerWithExternalCR.java
+++ bundle-generator/deployment/src/test/java/io/quarkiverse/operatorsdk/bundle/sources/ReconcilerWithExternalCR.java
@@ -1,13 +1,13 @@
 package io.quarkiverse.operatorsdk.bundle.sources;
 
 import io.javaoperatorsdk.operator.api.reconciler.Context;
-import io.javaoperatorsdk.operator.api.reconciler.ControllerConfiguration;
 import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
 import io.javaoperatorsdk.operator.api.reconciler.UpdateControl;
+import io.javaoperatorsdk.operator.api.reconciler.Workflow;
 import io.javaoperatorsdk.operator.api.reconciler.dependent.Dependent;
 import io.quarkiverse.operatorsdk.annotations.CSVMetadata;
 
-@ControllerConfiguration(dependents = @Dependent(type = ExternalDependentResource.class))
+@Workflow(dependents = @Dependent(type = ExternalDependentResource.class))
 @CSVMetadata(requiredCRDs = @CSVMetadata.RequiredCRD(kind = V1Beta1CRD.KIND, name = V1Beta1CRD.CR_NAME, version = V1Beta1CRD.VERSION))
 public class ReconcilerWithExternalCR implements Reconciler<First> {
     @Override
