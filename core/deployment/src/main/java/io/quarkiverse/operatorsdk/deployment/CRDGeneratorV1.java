657aa7780cc24cd7e1a2c296b1ed8958404b0756
Chris Laprun <claprun@redhat.com>
2024-11-22 18:09:03 +0100

    feat: make it possible to revert to using the v1 CRD generator
    Note that this should only be used if a blocking issue is found with the
    v2 generator because the v1 version won't be supported moving forward.
    
    Signed-off-by: Chris Laprun <claprun@redhat.com>

---
+++ core/deployment/src/main/java/io/quarkiverse/operatorsdk/deployment/CRDGeneratorV1.java
@@ -0,0 +1,46 @@
+package io.quarkiverse.operatorsdk.deployment;
+
+import java.io.File;
+import java.util.List;
+import java.util.Set;
+
+import org.jboss.logging.Logger;
+
+import io.fabric8.crd.generator.CustomResourceInfo;
+import io.fabric8.kubernetes.client.CustomResource;
+import io.quarkiverse.operatorsdk.runtime.CRDInfo;
+import io.quarkiverse.operatorsdk.runtime.CRDInfos;
+
+class CRDGeneratorV1 implements CRDGenerator {
+    private static final Logger log = Logger.getLogger(CRDGeneratorV1.class.getName());
+    private final io.fabric8.crd.generator.CRDGenerator generator;
+
+    public CRDGeneratorV1(boolean parallelGeneration) {
+        this.generator = new io.fabric8.crd.generator.CRDGenerator().withParallelGenerationEnabled(parallelGeneration);
+    }
+
+    @Override
+    public void generate(List<String> crdSpecVersions, File outputDir, Set<String> generated, CRDInfos converted) {
+        final var info = generator.forCRDVersions(crdSpecVersions).inOutputDir(outputDir).detailedGenerate();
+        final var crdDetailsPerNameAndVersion = info.getCRDDetailsPerNameAndVersion();
+
+        crdDetailsPerNameAndVersion.forEach((crdName, initialVersionToCRDInfoMap) -> {
+            log.infov("Generated {0} CRD:", crdName);
+            generated.add(crdName);
+
+            initialVersionToCRDInfoMap
+                    .forEach((crdSpecVersion, crdInfo) -> {
+                        final var filePath = crdInfo.getFilePath();
+                        log.infov("  - {0} -> {1}", crdSpecVersion, filePath);
+                        converted.addCRDInfo(new CRDInfo(crdName,
+                                crdSpecVersion, filePath, crdInfo.getDependentClassNames()));
+                    });
+        });
+    }
+
+    @Override
+    public void scheduleForGeneration(Class<? extends CustomResource<?, ?>> crClass) {
+        final var info = CustomResourceInfo.fromClass(crClass);
+        generator.customResources(info);
+    }
+}
