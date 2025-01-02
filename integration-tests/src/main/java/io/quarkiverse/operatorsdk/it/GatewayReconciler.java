5cfdc22f04efe53a52c4b7a5522b8f5682838202
Chris Laprun <claprun@redhat.com>
2025-01-02 15:49:37 +0100

    wip: check Fabric8 7 compatibility with Quarkus
    Signed-off-by: Chris Laprun <claprun@redhat.com>

--- integration-tests/src/main/java/io/quarkiverse/operatorsdk/it/GatewayReconciler.java
+++ integration-tests/src/main/java/io/quarkiverse/operatorsdk/it/GatewayReconciler.java
@@ -1,6 +1,6 @@
 package io.quarkiverse.operatorsdk.it;
 
-import io.fabric8.istio.api.networking.v1beta1.Gateway;
+import io.fabric8.kubernetes.api.model.gatewayapi.v1.Gateway;
 import io.javaoperatorsdk.operator.api.reconciler.Context;
 import io.javaoperatorsdk.operator.api.reconciler.Reconciler;
 import io.javaoperatorsdk.operator.api.reconciler.UpdateControl;
