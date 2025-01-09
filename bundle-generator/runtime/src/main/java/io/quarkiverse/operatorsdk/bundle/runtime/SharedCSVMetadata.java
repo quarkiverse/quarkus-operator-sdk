228f78fc962e59a60bd0ce3b9cd0846867ba9586
Chris Laprun <claprun@redhat.com>
2025-01-09 18:36:04 +0100

    chore: remove annotations that got moved to annotations module
    Signed-off-by: Chris Laprun <claprun@redhat.com>

--- bundle-generator/runtime/src/main/java/io/quarkiverse/operatorsdk/bundle/runtime/SharedCSVMetadata.java
+++
@@ -1,9 +0,0 @@
-package io.quarkiverse.operatorsdk.bundle.runtime;
-
-@SuppressWarnings("unused")
-@Deprecated(since = "6.3.1", forRemoval = true)
-/**
- * @deprecated Use {@link io.quarkiverse.operatorsdk.annotations.CSVMetadata} instead
- */
-public interface SharedCSVMetadata {
-}
