37eeef4cdc64c8c88e7e5586ebaace61f86f9125
Chris Laprun <claprun@redhat.com>
2025-01-09 14:54:22 +0100

    chore: remove deprecated KubernetesClientSerializationCustomizer
    Signed-off-by: Chris Laprun <claprun@redhat.com>

--- core/runtime/src/main/java/io/quarkiverse/operatorsdk/runtime/KubernetesClientObjectMapperCustomizer.java
+++ core/runtime/src/main/java/io/quarkiverse/operatorsdk/runtime/KubernetesClientObjectMapperCustomizer.java
@@ -5,9 +5,6 @@ import jakarta.inject.Singleton;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
-import io.quarkus.arc.Arc;
-import io.quarkus.jackson.ObjectMapperCustomizer;
-
 @Singleton
 public class KubernetesClientObjectMapperCustomizer implements
         io.quarkus.kubernetes.client.KubernetesClientObjectMapperCustomizer {
@@ -16,12 +13,5 @@ public class KubernetesClientObjectMapperCustomizer implements
     public void customize(ObjectMapper mapper) {
         // disable failure on empty beans
         mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
-
-        // apply now deprecated user-defined customizations
-        Arc.container()
-                .select(ObjectMapperCustomizer.class, KubernetesClientSerializationCustomizer.Literal.INSTANCE)
-                .stream()
-                .sorted()
-                .forEach(c -> c.customize(mapper));
     }
 }
