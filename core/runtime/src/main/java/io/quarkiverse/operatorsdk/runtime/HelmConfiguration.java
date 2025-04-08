449af47b6e4c6d6c65de4c8c7547ef8e17dea322
Chris Laprun <claprun@redhat.com>
2025-04-08 15:37:10 +0200

    feat: configure Helm chart name with quarkus.operator-sdk.helm.name
    Fixes #1075

--- core/runtime/src/main/java/io/quarkiverse/operatorsdk/runtime/HelmConfiguration.java
+++ core/runtime/src/main/java/io/quarkiverse/operatorsdk/runtime/HelmConfiguration.java
@@ -1,5 +1,7 @@
 package io.quarkiverse.operatorsdk.runtime;
 
+import java.util.Optional;
+
 import io.quarkus.runtime.annotations.ConfigGroup;
 import io.smallrye.config.WithDefault;
 
@@ -7,9 +9,14 @@ import io.smallrye.config.WithDefault;
 public interface HelmConfiguration {
 
     /**
-     * Can be used to disable helm chart generation.
+     * Can be used to disable Helm chart generation.
      */
     @WithDefault("false")
     Boolean enabled();
 
+    /**
+     * Specifies the name of the generated Helm chart. This will generate the Helm chart in {@code target/helm/<name>} instead
+     * of {@code target/helm} directly, if specified.
+     */
+    Optional<String> name();
 }
