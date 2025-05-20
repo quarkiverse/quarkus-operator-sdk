ae4a34a5f6b66436dc655401a24bf966c90de1a5
xstefank <xstefank122@gmail.com>
2025-05-20 12:31:07 +0200

    fix: allow to add labels in CSVMetadata
    Fixes #1088
    
    Signed-off-by: xstefank <xstefank122@gmail.com>

--- bundle-generator/deployment/src/test/java/io/quarkiverse/operatorsdk/bundle/sources/ThirdReconciler.java
+++ bundle-generator/deployment/src/test/java/io/quarkiverse/operatorsdk/bundle/sources/ThirdReconciler.java
@@ -9,6 +9,7 @@ import io.javaoperatorsdk.operator.api.reconciler.dependent.Dependent;
 import io.quarkiverse.operatorsdk.annotations.CSVMetadata;
 import io.quarkiverse.operatorsdk.annotations.CSVMetadata.Annotations;
 import io.quarkiverse.operatorsdk.annotations.CSVMetadata.Annotations.Annotation;
+import io.quarkiverse.operatorsdk.annotations.CSVMetadata.Label;
 import io.quarkiverse.operatorsdk.annotations.CSVMetadata.RequiredCRD;
 
 @Workflow(dependents = {
@@ -17,7 +18,9 @@ import io.quarkiverse.operatorsdk.annotations.CSVMetadata.RequiredCRD;
         @Dependent(name = "pod2", type = PodDependentResource.class)
 })
 @CSVMetadata(bundleName = ThirdReconciler.BUNDLE_NAME, requiredCRDs = @RequiredCRD(kind = SecondExternal.KIND, name = "externalagains."
-        + SecondExternal.GROUP, version = SecondExternal.VERSION), replaces = "1.0.0", annotations = @Annotations(skipRange = ">=1.0.0 <1.0.3", capabilities = "Test", repository = "should be overridden by property", others = @Annotation(name = "foo", value = "bar")))
+        + SecondExternal.GROUP, version = SecondExternal.VERSION), replaces = "1.0.0", annotations = @Annotations(skipRange = ">=1.0.0 <1.0.3", capabilities = "Test", repository = "should be overridden by property", others = @Annotation(name = "foo", value = "bar")), labels = {
+                @Label(name = "test-label", value = "test-value"),
+                @Label(name = "test-label2", value = "test-value2") })
 @ControllerConfiguration(name = ThirdReconciler.NAME)
 public class ThirdReconciler implements Reconciler<Third> {
 
