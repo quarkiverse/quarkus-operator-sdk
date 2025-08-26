bff23bc978aff1f38f5dedd5423c46f6c96c82cd
Chris Laprun <claprun@redhat.com>
2025-08-26 19:16:53 +0200

    fix: override getSecondaryResource to properly get associated resource
    Also improved test.

--- samples/mysql-schema/src/test/java/io/quarkiverse/operatorsdk/samples/mysqlschema/MySQLSchemaOperatorE2ETest.java
+++ samples/mysql-schema/src/test/java/io/quarkiverse/operatorsdk/samples/mysqlschema/MySQLSchemaOperatorE2ETest.java
@@ -7,11 +7,14 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.startsWith;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+
+import java.util.Optional;
 
 import jakarta.inject.Inject;
 
@@ -23,6 +26,7 @@ import org.junit.jupiter.api.Test;
 import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.javaoperatorsdk.operator.Operator;
+import io.quarkiverse.operatorsdk.samples.mysqlschema.schema.Schema;
 import io.quarkiverse.operatorsdk.samples.mysqlschema.schema.SchemaService;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.mockito.InjectSpy;
@@ -31,6 +35,8 @@ import io.quarkus.test.junit.mockito.InjectSpy;
 class MySQLSchemaOperatorE2ETest {
 
     final static Logger log = Logger.getLogger(MySQLSchemaOperatorE2ETest.class);
+    private static final String DB_NAME = "mydb1";
+    private static final String ENCODING = "utf8";
 
     @InjectSpy
     SchemaService schemaService;
@@ -54,19 +60,24 @@ class MySQLSchemaOperatorE2ETest {
     @Test
     void test() {
 
+        final Optional<Schema> maybeSchema = schemaService.getSchema(DB_NAME);
+        if (maybeSchema.isPresent()) {
+            fail("Schema " + DB_NAME + " should not already exist");
+        }
+
         MySQLSchema testSchema = new MySQLSchema();
         testSchema.setMetadata(new ObjectMetaBuilder()
-                .withName("mydb1")
+                .withName(DB_NAME)
                 .withNamespace(client.getNamespace())
                 .build());
         testSchema.setSpec(new SchemaSpec());
-        testSchema.getSpec().setEncoding("utf8");
+        testSchema.getSpec().setEncoding(ENCODING);
 
         log.infof("Creating test MySQLSchema object: %s", testSchema);
         client.resource(testSchema).serverSideApply();
 
         log.info("Waiting 10 seconds for expected resources to be created and updated");
-        await().atMost(10, SECONDS).untilAsserted(() -> {
+        await().pollDelay(9, SECONDS).atMost(10, SECONDS).untilAsserted(() -> {
             MySQLSchema updatedSchema = client.resources(MySQLSchema.class)
                     .inNamespace(testSchema.getMetadata().getNamespace())
                     .withName(testSchema.getMetadata().getName()).get();
@@ -77,7 +88,7 @@ class MySQLSchemaOperatorE2ETest {
             assertThat(updatedSchema.getStatus().getUrl(), startsWith("jdbc:mysql://"));
         });
 
-        verify(schemaService, times(1)).createSchemaAndRelatedUser(any(), eq("mydb1"), eq("utf8"), anyString(),
+        verify(schemaService, times(1)).createSchemaAndRelatedUser(any(), eq(DB_NAME), eq(ENCODING), anyString(),
                 anyString());
 
         client.resource(testSchema).delete();
@@ -95,6 +106,6 @@ class MySQLSchemaOperatorE2ETest {
                             assertThat(updatedSchema, is(nullValue()));
                         });
 
-        verify(schemaService, times(1)).deleteSchemaAndRelatedUser(any(), eq("mydb1"), anyString());
+        verify(schemaService, times(1)).deleteSchemaAndRelatedUser(any(), eq(DB_NAME), anyString());
     }
 }
