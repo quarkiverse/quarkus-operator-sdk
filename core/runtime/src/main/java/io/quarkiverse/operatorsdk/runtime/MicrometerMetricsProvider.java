6153ae6bbfea5ac44f0d47038d3fdbcdaeda8bad
Chris Laprun <metacosm@gmail.com>
2026-03-16 19:36:32 +0100

    feat: make it possible to revert to use v1 Metrics implementation

--- core/runtime/src/main/java/io/quarkiverse/operatorsdk/runtime/MicrometerMetricsProvider.java
+++ core/runtime/src/main/java/io/quarkiverse/operatorsdk/runtime/MicrometerMetricsProvider.java
@@ -1,9 +1,11 @@
 package io.quarkiverse.operatorsdk.runtime;
 
 import jakarta.enterprise.inject.Produces;
+import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 
 import io.javaoperatorsdk.operator.api.monitoring.Metrics;
+import io.javaoperatorsdk.operator.monitoring.micrometer.MicrometerMetrics;
 import io.javaoperatorsdk.operator.monitoring.micrometer.MicrometerMetricsV2;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.binder.MeterBinder;
@@ -12,10 +14,14 @@ import io.quarkus.arc.DefaultBean;
 @Singleton
 public class MicrometerMetricsProvider implements MeterBinder {
     private Metrics metrics = Metrics.NOOP;
+    @Inject
+    BuildTimeOperatorConfiguration buildTimeOperatorConfiguration;
 
     @Override
     public void bindTo(MeterRegistry registry) {
-        metrics = MicrometerMetricsV2.newBuilder(registry).build();
+        metrics = buildTimeOperatorConfiguration.useV1Metrics()
+                ? MicrometerMetrics.newPerResourceCollectingMicrometerMetricsBuilder(registry).build()
+                : MicrometerMetricsV2.newBuilder(registry).build();
     }
 
     @Produces
