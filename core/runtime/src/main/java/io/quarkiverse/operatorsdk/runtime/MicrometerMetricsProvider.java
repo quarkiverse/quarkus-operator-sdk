2fe0aa8207cba37af2a6318885b371825665a9c2
Martin Stefanko <xstefank122@gmail.com>
2026-07-03 08:42:31 +0200

    fix: inject MeterRegistry directly in MicrometerMetricsProvider to avoid NOOP metrics (#1389)
    Signed-off-by: xstefank <xstefank122@gmail.com>

--- core/runtime/src/main/java/io/quarkiverse/operatorsdk/runtime/MicrometerMetricsProvider.java
+++ core/runtime/src/main/java/io/quarkiverse/operatorsdk/runtime/MicrometerMetricsProvider.java
@@ -1,5 +1,6 @@
 package io.quarkiverse.operatorsdk.runtime;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.inject.Produces;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
@@ -8,17 +9,21 @@ import io.javaoperatorsdk.operator.api.monitoring.Metrics;
 import io.javaoperatorsdk.operator.monitoring.micrometer.MicrometerMetrics;
 import io.javaoperatorsdk.operator.monitoring.micrometer.MicrometerMetricsV2;
 import io.micrometer.core.instrument.MeterRegistry;
-import io.micrometer.core.instrument.binder.MeterBinder;
 import io.quarkus.arc.DefaultBean;
 
 @Singleton
-public class MicrometerMetricsProvider implements MeterBinder {
-    private Metrics metrics = Metrics.NOOP;
+public class MicrometerMetricsProvider {
+
     @Inject
     BuildTimeOperatorConfiguration buildTimeOperatorConfiguration;
 
-    @Override
-    public void bindTo(MeterRegistry registry) {
+    @Inject
+    MeterRegistry registry;
+
+    private Metrics metrics;
+
+    @PostConstruct
+    void init() {
         metrics = buildTimeOperatorConfiguration.useV1Metrics()
                 ? MicrometerMetrics.newPerResourceCollectingMicrometerMetricsBuilder(registry).build()
                 : MicrometerMetricsV2.newBuilder(registry).build();
